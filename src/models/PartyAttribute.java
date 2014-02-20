0522b4d274b66b3f31ff8770f69e57f88b055daa
rayliu <liuyubie@gmail.com>
2014-02-20 23:22:09 +0800

    房产客户 列表及 新增，还有不少需要改进的地方...

---
+++ src/models/PartyAttribute.java
@@ -0,0 +1,1 @@
+package models;import com.jfinal.plugin.activerecord.Model;public class PartyAttribute extends Model<PartyAttribute> {	public static final PartyAttribute dao = new PartyAttribute();}
