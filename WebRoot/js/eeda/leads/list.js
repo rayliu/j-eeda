1f63f38c70a7228fa2f3cb82009416dbc9370b5d
rayliu <liuyubie@gmail.com>
2014-03-26 21:22:23 +0800

    查找房源资料栏，重新加上“过滤条件”

--- WebRoot/js/eeda/leads/list.js
+++ WebRoot/js/eeda/leads/list.js
@@ -177,7 +177,7 @@ $(document).ready(function() {
 	//datatable, 动态处理
     var oTable = $('#eeda-table').dataTable({
         //"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-        "sDom": "<'row-fluid'<'span6'l><'span6'>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
         "sPaginationType": "bootstrap",
         "iDisplayLength": 10,
     	"oLanguage": {
