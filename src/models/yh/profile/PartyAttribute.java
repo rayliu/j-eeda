e767300fa81be41ac1656870dcf3eef5d7b08bf5
rayliu <liuyubie@gmail.com>
2014-04-07 16:01:00 +0800

    客户模块的 列表， 新增，编辑功能， 基本完成

---
+++ src/models/yh/profile/PartyAttribute.java
@@ -0,0 +1,1 @@
+package models.yh.profile;import com.jfinal.plugin.activerecord.Model;public class PartyAttribute extends Model<PartyAttribute> {	public static final PartyAttribute dao = new PartyAttribute();}
