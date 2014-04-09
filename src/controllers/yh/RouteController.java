0b082d097493a26f5e990eae7adbe7cda0af50f6
rayliu <liuyubie@gmail.com>
2014-04-09 22:44:13 +0800

    route 下拉列表获取a标签的值 , 回填到code, name中去。

--- src/controllers/yh/RouteController.java
+++ src/controllers/yh/RouteController.java
@@ -11,15 +11,18 @@ import com.jfinal.plugin.activerecord.Record;
 
 public class RouteController extends Controller {
     private Logger logger = Logger.getLogger(LoginUserController.class);
-    public  void  index(){
+
+    public void index() {
         render("/yh/profile/route/route.html");
     }
-    //链接到添加干线页面
-    public void editRoute(){
+
+    // 链接到添加干线页面
+    public void editRoute() {
         render("/yh/profile/route/edit.html");
     }
-  //编辑干线
-    public void edit(){
+
+    // 编辑干线
+    public void edit() {
         String id = getPara();
         if (id != null) {
             UserLogin l = UserLogin.dao.findById(id);
@@ -28,49 +31,56 @@ public class RouteController extends Controller {
             setAttr("ul", new UserLogin());
         }
         render("/yh/profile/loginUser/addUser.html");
-        
+
     }
-    //添加干线
-    public void saveRoute(){
-        /*if (!isAuthenticated())
-            return;
-*/
+
+    // 添加干线
+    public void saveRoute() {
+        /*
+         * if (!isAuthenticated()) return;
+         */
         String id = getPara("userId");
         if (id != "") {
             UserLogin user = UserLogin.dao.findById(id);
         }
-        Record user =new Record();
+        Record user = new Record();
         user.set("user_name", getPara("username"));
         user.set("password", getPara("password"));
         user.set("password_hint", getPara("pw_hint"));
         if (id != "") {
             logger.debug("update....");
             user.set("id", id);
-            Db.update("user_login",user);
+            Db.update("user_login", user);
         } else {
             logger.debug("insert....");
             Db.save("user_login", user);
         }
         render("/yh/profile/loginUser/loginUser.html");
-        
+
     }
-    //删除干线
-    public void del(){
-        /*UserLogin.dao.find("select * from user_login");
-        UserLogin.dao.deleteById(getParaToInt());*/
+
+    // 删除干线
+    public void del() {
+        /*
+         * UserLogin.dao.find("select * from user_login");
+         * UserLogin.dao.deleteById(getParaToInt());
+         */
         String id = getPara();
         if (id != null) {
-        UserLogin l = UserLogin.dao.findById(id);
-        l.delete();
+            UserLogin l = UserLogin.dao.findById(id);
+            l.delete();
         }
         render("/yh/profile/loginUser/loginUser.html");
     }
-    //input控件列出城市列表
-    public void search(){
-        String locationName=getPara("locationName");
-        //不能查所有
-        List<Record> locationList = Db.find("select * from location where name like '%"+locationName+"%'");
-        renderJson(locationList);        
-    }
 
+    // input控件列出城市列表
+    public void search() {
+        String locationName = getPara("locationName");
+
+        // 不能查所有
+        if (locationName.trim().length() > 0) {
+            List<Record> locationList = Db.find("select * from location where name like '%" + locationName + "%'");
+            renderJson(locationList);
+        }
+    }
 }
