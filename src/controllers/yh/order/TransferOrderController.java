c49d5ae2749e53add72044a61d7159c8841f0461
rayliu <liuyubie@gmail.com>
2014-04-23 23:14:27 +0800

    对“运输单”的新增页面进行了美化。

--- src/controllers/yh/order/TransferOrderController.java
+++ src/controllers/yh/order/TransferOrderController.java
@@ -18,143 +18,138 @@ import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 
 public class TransferOrderController extends Controller {
-	
+
     private Logger logger = Logger.getLogger(TransferOrderController.class);
-	Subject currentUser = SecurityUtils.getSubject();
-    
-	public void index() {
-		render("profile/transferOrder/transferOrderList.html");
-	}
+    Subject currentUser = SecurityUtils.getSubject();
 
-	public void list() {
-		/*
-		 * Paging
-		 */
-		String sLimit = "";
-		String pageIndex = getPara("sEcho");
-		if (getPara("iDisplayStart") != null
-				&& getPara("iDisplayLength") != null) {
-			sLimit = " LIMIT " + getPara("iDisplayStart") + ", "
-					+ getPara("iDisplayLength");
-		}
+    public void index() {
+        render("transferOrder/transferOrderList.html");
+    }
 
-		String sqlTotal = "select count(1) total from transfer_order";
-		Record rec = Db.findFirst(sqlTotal);
-		logger.debug("total records:" + rec.getLong("total"));
+    public void list() {
+        /*
+         * Paging
+         */
+        String sLimit = "";
+        String pageIndex = getPara("sEcho");
+        if (getPara("iDisplayStart") != null && getPara("iDisplayLength") != null) {
+            sLimit = " LIMIT " + getPara("iDisplayStart") + ", " + getPara("iDisplayLength");
+        }
 
-		String sql = "select * from transfer_order";
+        String sqlTotal = "select count(1) total from transfer_order";
+        Record rec = Db.findFirst(sqlTotal);
+        logger.debug("total records:" + rec.getLong("total"));
 
-		List<Record> transferOrders = Db.find(sql);
+        String sql = "select * from transfer_order";
 
-		Map transferOrderListMap = new HashMap();
-		transferOrderListMap.put("sEcho", pageIndex);
-		transferOrderListMap.put("iTotalRecords", rec.getLong("total"));
-		transferOrderListMap.put("iTotalDisplayRecords", rec.getLong("total"));
+        List<Record> transferOrders = Db.find(sql);
 
-		transferOrderListMap.put("aaData", transferOrders);
+        Map transferOrderListMap = new HashMap();
+        transferOrderListMap.put("sEcho", pageIndex);
+        transferOrderListMap.put("iTotalRecords", rec.getLong("total"));
+        transferOrderListMap.put("iTotalDisplayRecords", rec.getLong("total"));
 
-		renderJson(transferOrderListMap);
-	}
+        transferOrderListMap.put("aaData", transferOrders);
 
-	public void add() {
-		setAttr("saveOK", false);
-		render("profile/transferOrder/transferOrderEdit.html");
-	}
+        renderJson(transferOrderListMap);
+    }
 
-	public void edit() {
-		long id = getParaToLong();
+    public void add() {
+        setAttr("saveOK", false);
+        render("transferOrder/transferOrderEdit.html");
+    }
 
-		Party party = Party.dao.findById(id);
-		setAttr("party", party);
+    public void edit() {
+        long id = getParaToLong();
 
-		Contact contact = Contact.dao
-				.findFirst("select * from contact where id=?",
-						party.getLong("contact_id"));
-		setAttr("contact", contact);
+        Party party = Party.dao.findById(id);
+        setAttr("party", party);
 
-		render("profile/transferOrder/transferOrderEdit.html");
-	}
+        Contact contact = Contact.dao.findFirst("select * from contact where id=?", party.getLong("contact_id"));
+        setAttr("contact", contact);
 
-	public void delete() {
-		long id = getParaToLong();
+        render("transferOrder/transferOrderEdit.html");
+    }
 
-		Party party = Party.dao.findById(id);
-		party.delete();
+    public void delete() {
+        long id = getParaToLong();
 
-		Contact contact = Contact.dao
-				.findFirst("select * from contact where id=?",
-						party.getLong("contact_id"));
-		contact.delete();
+        Party party = Party.dao.findById(id);
+        party.delete();
 
-		redirect("/yh/transferOrder");
-	}
+        Contact contact = Contact.dao.findFirst("select * from contact where id=?", party.getLong("contact_id"));
+        contact.delete();
 
-	public void save() {
+        redirect("/yh/transferOrder");
+    }
 
-		String id = getPara("party_id");
-		Party party = null;
-		Contact contact = null;
-		Date createDate = Calendar.getInstance().getTime();
-		if (id != null && !id.equals("")) {
-			party = Party.dao.findById(id);
-			party.set("last_update_date", createDate).update();
+    public void save() {
 
-			contact = Contact.dao.findFirst("select * from contact where id=?",
-					party.getLong("contact_id"));
-			setContact(contact);
-			contact.update();
-		} else {
-			contact = new Contact();
-			setContact(contact);
-			contact.save();
-			party = new Party();
-			party.set("party_type", Party.PARTY_TYPE_SERVICE_PROVIDER);
-			party.set("contact_id", contact.getLong("id"));
-			party.set("creator", "test");
-			party.set("create_date", createDate);
-			party.save();
+        String id = getPara("party_id");
+        Party party = null;
+        Contact contact = null;
+        Date createDate = Calendar.getInstance().getTime();
+        if (id != null && !id.equals("")) {
+            party = Party.dao.findById(id);
+            party.set("last_update_date", createDate).update();
 
-		}
+            contact = Contact.dao.findFirst("select * from contact where id=?", party.getLong("contact_id"));
+            setContact(contact);
+            contact.update();
+        } else {
+            contact = new Contact();
+            setContact(contact);
+            contact.save();
+            party = new Party();
+            party.set("party_type", Party.PARTY_TYPE_SERVICE_PROVIDER);
+            party.set("contact_id", contact.getLong("id"));
+            party.set("creator", "test");
+            party.set("create_date", createDate);
+            party.save();
 
-		setAttr("saveOK", true);
-		render("profile/transferOrder/transferOrderList.html");
-	}
+        }
 
-	private void setContact(Contact contact) {
-		contact.set("company_name", getPara("company_name"));
-		contact.set("contact_person", getPara("contact_person"));
-		contact.set("email", getPara("email"));
-		contact.set("mobile", getPara("mobile"));
-		contact.set("phone", getPara("phone"));
-		contact.set("address", getPara("address"));
-		contact.set("city", getPara("city"));
-		contact.set("postal_code", getPara("postal_code"));
-	}
-	
-	// 客户列表,列出最近使用的5个客户
-	public void selectCustomer(){
-		List<Contact> contactjson = Contact.dao.find("select * from contact c  where id in (select contact_id from party where party_type='CUSTOMER' order by last_update_date desc limit 0,5)");			
+        setAttr("saveOK", true);
+        render("transferOrder/transferOrderList.html");
+    }
+
+    private void setContact(Contact contact) {
+        contact.set("company_name", getPara("company_name"));
+        contact.set("contact_person", getPara("contact_person"));
+        contact.set("email", getPara("email"));
+        contact.set("mobile", getPara("mobile"));
+        contact.set("phone", getPara("phone"));
+        contact.set("address", getPara("address"));
+        contact.set("city", getPara("city"));
+        contact.set("postal_code", getPara("postal_code"));
+    }
+
+    // 客户列表,列出最近使用的5个客户
+    public void selectCustomer() {
+        List<Contact> contactjson = Contact.dao
+                .find("select * from contact c  where id in (select contact_id from party where party_type='CUSTOMER' order by last_update_date desc limit 0,5)");
         renderJson(contactjson);
-	}
-	
-	// 保存客户 TODO 1.在新建页面不能直接显示,需重新进入才能显示刚添加的. 2.点击radio的label不能选中前面的按钮.	 3.显示与隐藏切换问题
-	// 4.最近使用的5个客户:第一次进来时没有信息怎么处理?
-	public void saveCustomer(){
-		Party party = new Party();
-		party.set("party_type", Party.PARTY_TYPE_CUSTOMER);
-		Contact contact = new Contact();
-		setContact(contact);
-		contact.save();
-		party.set("contact_id", contact.getLong("id"));
-		party.set("create_date", new Date());
-		party.set("creator", currentUser.getPrincipal());		
-		party.save();
-	}
+    }
 
-	// 收货人列表
-	public void selectContact(){
-		List<Contact> contacts = Contact.dao.find("select * from contact");			
-		renderJson(contacts);
-	}
-	
+    // 保存客户 TODO 1.在新建页面不能直接显示,需重新进入才能显示刚添加的. 2.点击radio的label不能选中前面的按钮.
+    // 3.显示与隐藏切换问题
+    // 4.最近使用的5个客户:第一次进来时没有信息怎么处理?
+    public void saveCustomer() {
+        Party party = new Party();
+        party.set("party_type", Party.PARTY_TYPE_CUSTOMER);
+        Contact contact = new Contact();
+        setContact(contact);
+        contact.save();
+        party.set("contact_id", contact.getLong("id"));
+        party.set("create_date", new Date());
+        party.set("creator", currentUser.getPrincipal());
+        party.save();
+    }
+
+    // 收货人列表
+    public void selectContact() {
+        List<Contact> contacts = Contact.dao.find("select * from contact");
+        renderJson(contacts);
+    }
+
 }
