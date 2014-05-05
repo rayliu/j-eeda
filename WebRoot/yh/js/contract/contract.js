ae06c281a4388980eb252c6013d062e3b3f7c838
jam <237116624@qq.com>
2014-05-05 18:01:08 +0800

    配送单 添加功能70%

--- WebRoot/yh/js/contract/contract.js
+++ WebRoot/yh/js/contract/contract.js
@@ -218,4 +218,39 @@ $(document).ready(function() {
 			$('#toName').val($(this).text());
         	$("#toLocationList").hide();
     	});
+		
+		//datatable, 动态处理
+	    $('#eeda-table').dataTable({
+	        //"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+	        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+	        //"sPaginationType": "bootstrap",
+	        "iDisplayLength": 10,
+	    	"oLanguage": {
+	            "sUrl": "/eeda/dataTables.ch.txt"
+	        },
+	        "sAjaxSource": "/yh/delivery/deliveryList",
+	        "aoColumns": [   
+	            
+	            {"mDataProp":"ORDER_NO"},
+	            {"mDataProp":"TRANSFER_ORDER_ID"},
+	            {"mDataProp":"CUSTOMER_ID"},        	
+	            {"mDataProp":"SP_ID"},
+	            {"mDataProp":"NOTIFY_PARTY_ID"},
+	            {"mDataProp":"STATUS"},
+	            { 
+	                "mDataProp": null, 
+	                "sWidth": "8%",                
+	                "fnRender": function(obj) {                    
+	                    return "<a class='btn btn-success ' href='/yh/transferOrder/edit/"+obj.aData.ID+"'>"+
+	                                "<i class='fa fa-edit fa-fw'></i>"+
+	                                "查看"+
+	                            "</a>"+
+	                            "<a class='btn btn-danger cancelbutton' code='"+obj.aData.ID+"'>"+
+	                                "<i class='fa fa-trash-o fa-fw'></i>"+ 
+	                                "取消"+
+	                            "</a>";
+	                }
+	            }                         
+	        ]      
+	    });	
     });
--- WebRoot/yh/js/delivery/delivery.js
+++ WebRoot/yh/js/delivery/delivery.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
 		  //获取供应商的list，选中信息在下方展示其他信息
 			$('#spMessage').on('keyup', function(){
 				var inputStr = $('#spMessage').val();
-				$.get('/yh/transferOrder/searchSp', {input:inputStr}, function(data){
+				$.get('/yh/delivery/searchSp', {input:inputStr}, function(data){
 					console.log(data);
 					var spList =$("#spList");
 					spList.empty();
 					for(var i = 0; i < data.length; i++)
 					{
-						spList.append("<li><a tabindex='-1' class='fromLocationItem' post_code='"+data[i].POSTAL_CODE+"' contact_person='"+data[i].CONTACT_PERSON+"' email='"+data[i].EMAIL+"' phone='"+data[i].PHONE+"' spid='"+data[i].ID+"' address='"+data[i].ADDRESS+"', company_name='"+data[i].COMPANY_NAME+"', >"+data[i].COMPANY_NAME+"</a></li>");
+						spList.append("<li><a tabindex='-1' class='fromLocationItem' code='"+data[i].PID+"' post_code='"+data[i].POSTAL_CODE+"' contact_person='"+data[i].CONTACT_PERSON+"' email='"+data[i].EMAIL+"' phone='"+data[i].PHONE+"' spid='"+data[i].ID+"' address='"+data[i].ADDRESS+"', company_name='"+data[i].COMPANY_NAME+"', >"+data[i].COMPANY_NAME+"</a></li>");
 					}
 				},'json');
 
@@ -21,8 +21,10 @@ $(document).ready(function() {
 			$('#spList').on('click', '.fromLocationItem', function(e){
 				$('#spMessage').val($(this).text());
 				$('#sp_id').val($(this).attr('spid'));
+				$('#cid').val($(this).attr('code'));
 				var pageSpName = $("#pageSpName");
 				pageSpName.empty();
+				
 				pageSpName.append($(this).attr('contact_person')+'&nbsp;');
 				pageSpName.append($(this).attr('phone')); 
 				var pageSpAddress = $("#pageSpAddress");
@@ -30,5 +32,26 @@ $(document).ready(function() {
 				pageSpAddress.append($(this).attr('address'));
 		        $('#spList').hide();
 		    }); 
+			//datatable, 动态处理
+			
+		    $('#eeda-table').dataTable({
+		        //"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+		        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+		        //"sPaginationType": "bootstrap",
+		        "iDisplayLength": 10,
+		    	"oLanguage": {
+		            "sUrl": "/eeda/dataTables.ch.txt"
+		        },
+		        "sAjaxSource": "/yh/delivery/transferOderItem",
+		        "aoColumns": [   
+		            
+		            {"mDataProp":"ITEM_NO"},
+		            {"mDataProp":"ITEM_NAME"},
+		            {"mDataProp":"MOUNT"},        	
+		            {"mDataProp":"UNIT"},
+		            {"mDataProp":"ITEM_DESC"},
+		            {"mDataProp":"REMARK"},
+		        ]      
+		    });	
 			
 });
