f80cc2a5f1e335f225b8cd5a55050d9ba9b34df2
Xavier <643751802@qq.com>
2014-05-29 20:54:54 +0800

    角色对应权限后台不能获取前台构造的数组

--- src/config/EedaConfig.java
+++ src/config/EedaConfig.java
@@ -57,179 +57,203 @@ import controllers.eeda.SalesOrderController;
 import controllers.eeda.UserProfileController;
 
 public class EedaConfig extends JFinalConfig {
-    private static String H2 = "H2";
-    private static String Mysql = "Mysql";
-    private static String ProdMysql = "ProdMysql";
-    /**
-     * 
-     * 供Shiro插件使用 。
-     */
-    Routes routes;
+	private static String H2 = "H2";
+	private static String Mysql = "Mysql";
+	private static String ProdMysql = "ProdMysql";
+	/**
+	 * 
+	 * 供Shiro插件使用 。
+	 */
+	Routes routes;
 
-    C3p0Plugin cp;
-    ActiveRecordPlugin arp;
+	C3p0Plugin cp;
+	ActiveRecordPlugin arp;
 
-    public void configConstant(Constants me) {
+	public void configConstant(Constants me) {
 
-        me.setDevMode(true);
+		me.setDevMode(true);
 
-        BeetlRenderFactory templateFactory = new BeetlRenderFactory();
-        me.setMainRenderFactory(templateFactory);
+		BeetlRenderFactory templateFactory = new BeetlRenderFactory();
+		me.setMainRenderFactory(templateFactory);
 
-        templateFactory.groupTemplate.setCharset("utf-8");// 没有这句，html上的汉字会乱码
+		templateFactory.groupTemplate.setCharset("utf-8");// 没有这句，html上的汉字会乱码
 
-        // 注册后，可以使beetl html中使用shiro tag
+		// 注册后，可以使beetl html中使用shiro tag
         templateFactory.groupTemplate.registerFunctionPackage("shiro", new ShiroExt());
 
-        // me.setErrorView(401, "/login.html");
-        // me.setErrorView(403, "/login.html");
-        me.setError404View("/404.html");
-        // me.setError500View("/login.html");
-        // me.setErrorView(503, "/login.html");
-        // get name representing the running Java virtual machine.
-        String name = ManagementFactory.getRuntimeMXBean().getName();
-        System.out.println(name);
-        // get pid
-        String pid = name.split("@")[0];
-        System.out.println("Pid is: " + pid);
-    }
+		// me.setErrorView(401, "/login.html");
+		// me.setErrorView(403, "/login.html");
+		me.setError404View("/404.html");
+		// me.setError500View("/login.html");
+		// me.setErrorView(503, "/login.html");
+		// get name representing the running Java virtual machine.
+		String name = ManagementFactory.getRuntimeMXBean().getName();
+		System.out.println(name);
+		// get pid
+		String pid = name.split("@")[0];
+		System.out.println("Pid is: " + pid);
+	}
 
-    public void configRoute(Routes me) {
-        this.routes = me;
+	public void configRoute(Routes me) {
+		this.routes = me;
 
-        // eeda project controller
-        me.add("/", AppController.class);
-        me.add("/case", CaseController.class);
-        me.add("/user", UserProfileController.class);
-        me.add("/salesOrder", SalesOrderController.class);
-        me.add("/loan", LoanController.class);
-        me.add("/propertyClient", PropertyClientController.class);
-        // me.add("/au", AdminUserController.class);
+		// eeda project controller
+		me.add("/", AppController.class);
+		me.add("/case", CaseController.class);
+		me.add("/user", UserProfileController.class);
+		me.add("/salesOrder", SalesOrderController.class);
+		me.add("/loan", LoanController.class);
+		me.add("/propertyClient", PropertyClientController.class);
+		// me.add("/au", AdminUserController.class);
 
         // me.add("/fileUpload", HelloController.class);
 
-        // yh project controller
-        me.add("/yh", controllers.yh.AppController.class, "/yh");
-        me.add("/yh/loginUser", controllers.yh.LoginUserController.class, "/yh");
-        me.add("/yh/role", controllers.yh.RoleController.class, "/yh");
-        me.add("/yh/toll", controllers.yh.TollController.class, "/yh");
-        me.add("/yh/privilege", controllers.yh.PrivilegeController.class, "/yh");
-        me.add("/yh/pay", controllers.yh.PayController.class, "/yh");
-        me.add("/yh/customer", controllers.yh.profile.CustomerController.class, "/yh");
-        me.add("/yh/serviceProvider", controllers.yh.profile.ServiceProviderController.class, "/yh");
-        me.add("/yh/location", controllers.yh.LocationController.class, "/yh");
-        me.add("/yh/customerContract", controllers.yh.contract.ContractController.class, "/yh");
-        me.add("/yh/spContract", controllers.yh.contract.ContractController.class, "/yh");
-        me.add("/yh/route", controllers.yh.RouteController.class, "/yh");
-        me.add("/yh/office", controllers.yh.OfficeController.class, "/yh");
-        me.add("/yh/product", controllers.yh.profile.ProductController.class, "/yh");
-        me.add("/yh/warehouse", controllers.yh.profile.WarehouseController.class, "/yh");
-        me.add("/yh/orderStatus", controllers.yh.profile.OrderStatusController.class, "/yh");
-        me.add("/yh/account", controllers.yh.AccountController.class, "/yh");
-        me.add("/yh/transferOrder", controllers.yh.order.TransferOrderController.class, "/yh");
-        me.add("/yh/transferOrderItem", controllers.yh.order.TransferOrderItemController.class, "/yh");
-        me.add("/yh/transferOrderItemDetail", controllers.yh.order.TransferOrderItemDetailController.class, "/yh");
-        me.add("/yh/transferOrderMilestone", controllers.yh.order.TransferOrderMilestoneController.class, "/yh");
-        me.add("/yh/returnorder", controllers.yh.ReturnOrderControllers.class, "/yh");
-        me.add("/yh/delivery", controllers.yh.delivery.DeliveryController.class, "/yh");
-        me.add("/yh/deliveryOrderMilestone", controllers.yh.delivery.DeliveryOrderMilestoneController.class, "/yh");
-        me.add("/yh/pickupOrder", controllers.yh.pickup.PickupOrderController.class, "/yh");
-        me.add("/yh/chargeCheckOrder", controllers.yh.arap.ChargeCheckOrderController.class, "/yh");
-        me.add("/yh/paymentCheckOrder", controllers.yh.arap.PaymentCheckOrderController.class, "/yh");
-        me.add("/yh/receivableCheckOrder", controllers.yh.arap.ReceivableCheckOrderController.class, "/yh");
-        me.add("/yh/copeCheckOrder", controllers.yh.arap.CopeCheckOrderController.class, "/yh");
-        me.add("/yh/departureOrder", controllers.yh.departure.DepartureOrderController.class, "/yh");
-    }
+		// yh project controller
+		me.add("/yh", controllers.yh.AppController.class, "/yh");
+		me.add("/yh/loginUser", controllers.yh.LoginUserController.class, "/yh");
+		me.add("/yh/role", controllers.yh.RoleController.class, "/yh");
+		me.add("/yh/toll", controllers.yh.TollController.class, "/yh");
+		me.add("/yh/privilege", controllers.yh.PrivilegeController.class, "/yh");
+		me.add("/yh/pay", controllers.yh.PayController.class, "/yh");
+		me.add("/yh/customer", controllers.yh.profile.CustomerController.class,
+		        "/yh");
+		me.add("/yh/serviceProvider",
+		        controllers.yh.profile.ServiceProviderController.class, "/yh");
+		me.add("/yh/location", controllers.yh.LocationController.class, "/yh");
+		me.add("/yh/customerContract",
+		        controllers.yh.contract.ContractController.class, "/yh");
+		me.add("/yh/spContract",
+		        controllers.yh.contract.ContractController.class, "/yh");
+		me.add("/yh/route", controllers.yh.RouteController.class, "/yh");
+		me.add("/yh/office", controllers.yh.OfficeController.class, "/yh");
+		me.add("/yh/product", controllers.yh.profile.ProductController.class,
+		        "/yh");
+		me.add("/yh/warehouse",
+		        controllers.yh.profile.WarehouseController.class, "/yh");
+		me.add("/yh/orderStatus",
+		        controllers.yh.profile.OrderStatusController.class, "/yh");
+		me.add("/yh/account", controllers.yh.AccountController.class, "/yh");
+		me.add("/yh/transferOrder",
+		        controllers.yh.order.TransferOrderController.class, "/yh");
+		me.add("/yh/transferOrderItem",
+		        controllers.yh.order.TransferOrderItemController.class, "/yh");
+		me.add("/yh/transferOrderItemDetail",
+		        controllers.yh.order.TransferOrderItemDetailController.class,
+		        "/yh");
+		me.add("/yh/transferOrderMilestone",
+		        controllers.yh.order.TransferOrderMilestoneController.class,
+		        "/yh");
+		me.add("/yh/returnorder", controllers.yh.ReturnOrderControllers.class,
+		        "/yh");
+		me.add("/yh/delivery",
+		        controllers.yh.delivery.DeliveryController.class, "/yh");
+		me.add("/yh/deliveryOrderMilestone",
+		        controllers.yh.delivery.DeliveryOrderMilestoneController.class,
+		        "/yh");
+		me.add("/yh/pickupOrder",
+		        controllers.yh.pickup.PickupOrderController.class, "/yh");
+		me.add("/yh/chargeCheckOrder",
+		        controllers.yh.arap.ChargeCheckOrderController.class, "/yh");
+		me.add("/yh/paymentCheckOrder",
+		        controllers.yh.arap.PaymentCheckOrderController.class, "/yh");
+		me.add("/yh/receivableCheckOrder",
+		        controllers.yh.arap.ReceivableCheckOrderController.class, "/yh");
+		me.add("/yh/copeCheckOrder",
+		        controllers.yh.arap.CopeCheckOrderController.class, "/yh");
+		me.add("/yh/departureOrder",
+		        controllers.yh.departure.DepartureOrderController.class, "/yh");
+	}
 
-    public void configPlugin(Plugins me) {
-        // 加载Shiro插件, for backend notation, not for UI
-        me.add(new ShiroPlugin(routes));
+	public void configPlugin(Plugins me) {
+		// 加载Shiro插件, for backend notation, not for UI
+		me.add(new ShiroPlugin(routes));
 
-        loadPropertyFile("app_config.txt");
+		loadPropertyFile("app_config.txt");
 
-        // H2 or mysql
-        initDBconnector();
+		// H2 or mysql
+		initDBconnector();
 
-        me.add(cp);
+		me.add(cp);
 
-        arp = new ActiveRecordPlugin(cp);
-        arp.setShowSql(true);// ShowSql
-        me.add(arp);
+		arp = new ActiveRecordPlugin(cp);
+		arp.setShowSql(true);// ShowSql
+		me.add(arp);
 
-        arp.setDialect(new MysqlDialect());
-        // 配置属性名(字段名)大小写不敏感容器工厂
-        arp.setContainerFactory(new CaseInsensitiveContainerFactory());
+		arp.setDialect(new MysqlDialect());
+		// 配置属性名(字段名)大小写不敏感容器工厂
+		arp.setContainerFactory(new CaseInsensitiveContainerFactory());
 
-        arp.addMapping("leads", Leads.class);
-        arp.addMapping("support_case", Case.class);
-        arp.addMapping("user_login", UserLogin.class);
-        arp.addMapping("order_header", Order.class);
-        arp.addMapping("order_item", OrderItem.class);
-        arp.addMapping("party", Party.class);
-        arp.addMapping("party_attribute", PartyAttribute.class);
-        arp.addMapping("contact", Contact.class);
-        arp.addMapping("office", Office.class);
-        arp.addMapping("fin_account", Account.class);
-        arp.addMapping("role_table", Role.class);
-        arp.addMapping("Fin_item", Toll.class);
-        arp.addMapping("privilege_table", Privilege.class);
-        arp.addMapping("route", Route.class);
-        arp.addMapping("product", Product.class);
-        arp.addMapping("warehouse", Warehouse.class);
-        arp.addMapping("contract_item", ContractItem.class);
-        arp.addMapping("order_status", OrderStatus.class);
-        arp.addMapping("contract", Contract.class);
-        arp.addMapping("transfer_order", TransferOrder.class);
-        arp.addMapping("transfer_order_item", TransferOrderItem.class);
-        arp.addMapping("transfer_order_item_detail", TransferOrderItemDetail.class);
-        arp.addMapping("return_order", ReturnOrder.class);
-        arp.addMapping("delivery_order", DeliveryOrder.class);
-        arp.addMapping("transfer_order_milestone", TransferOrderMilestone.class);
-        arp.addMapping("billing_order", BillingOrder.class);
-        arp.addMapping("billing_order_item", BillingOrderItem.class);
-        arp.addMapping("delivery_order_milestone", DeliveryOrderMilestone.class);
-        arp.addMapping("location", Location.class);
+		arp.addMapping("leads", Leads.class);
+		arp.addMapping("support_case", Case.class);
+		arp.addMapping("user_login", UserLogin.class);
+		arp.addMapping("order_header", Order.class);
+		arp.addMapping("order_item", OrderItem.class);
+		arp.addMapping("party", Party.class);
+		arp.addMapping("party_attribute", PartyAttribute.class);
+		arp.addMapping("contact", Contact.class);
+		arp.addMapping("office", Office.class);
+		arp.addMapping("fin_account", Account.class);
+		arp.addMapping("role", Role.class);
+		arp.addMapping("Fin_item", Toll.class);
+		arp.addMapping("privileges", Privilege.class);
+		arp.addMapping("route", Route.class);
+		arp.addMapping("product", Product.class);
+		arp.addMapping("warehouse", Warehouse.class);
+		arp.addMapping("contract_item", ContractItem.class);
+		arp.addMapping("order_status", OrderStatus.class);
+		arp.addMapping("contract", Contract.class);
+		arp.addMapping("transfer_order", TransferOrder.class);
+		arp.addMapping("transfer_order_item", TransferOrderItem.class);
+		arp.addMapping("transfer_order_item_detail",
+		        TransferOrderItemDetail.class);
+		arp.addMapping("return_order", ReturnOrder.class);
+		arp.addMapping("delivery_order", DeliveryOrder.class);
+		arp.addMapping("transfer_order_milestone", TransferOrderMilestone.class);
+		arp.addMapping("billing_order", BillingOrder.class);
+		arp.addMapping("billing_order_item", BillingOrderItem.class);
+		arp.addMapping("delivery_order_milestone", DeliveryOrderMilestone.class);
+		arp.addMapping("location", Location.class);
         arp.addMapping("fin_account_item",AccountItem.class);
         
-        // yh mapping
+		// yh mapping
 
-    }
+	}
 
-    private void initDBconnector() {
-        String dbType = getProperty("dbType");
-        String url = getProperty("dbUrl");
-        String username = getProperty("username");
-        String pwd = getProperty("pwd");
+	private void initDBconnector() {
+		String dbType = getProperty("dbType");
+		String url = getProperty("dbUrl");
+		String username = getProperty("username");
+		String pwd = getProperty("pwd");
 
-        if (H2.equals(dbType)) {
-            connectH2();
-        } else {
-            cp = new C3p0Plugin(url, username, pwd);
-        }
+		if (H2.equals(dbType)) {
+			connectH2();
+		} else {
+			cp = new C3p0Plugin(url, username, pwd);
+		}
 
-    }
+	}
 
-    private void connectH2() {
-        // 这个启动web console以方便通过localhost:8082访问数据库
-        try {
-            Server.createWebServer().start();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+	private void connectH2() {
+		// 这个启动web console以方便通过localhost:8082访问数据库
+		try {
+			Server.createWebServer().start();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-        cp = new C3p0Plugin("jdbc:h2:mem:eeda;", "sa", "");
-        cp.setDriverClass("org.h2.Driver");
-        DataInitUtil.initH2Tables(cp);
-    }
+		cp = new C3p0Plugin("jdbc:h2:mem:eeda;", "sa", "");
+		cp.setDriverClass("org.h2.Driver");
+		DataInitUtil.initH2Tables(cp);
+	}
 
-    public void configInterceptor(Interceptors me) {
-        // me.add(new ShiroInterceptor());
+	public void configInterceptor(Interceptors me) {
+		// me.add(new ShiroInterceptor());
 
-    }
+	}
 
-    public void configHandler(Handlers me) {
-        if (H2.equals(getProperty("dbType"))) {
-            DataInitUtil.initData(cp);
-        }
-    }
+	public void configHandler(Handlers me) {
+		if (H2.equals(getProperty("dbType"))) {
+			DataInitUtil.initData(cp);
+		}
+	}
 }
--- src/controllers/yh/PrivilegeController.java
+++ src/controllers/yh/PrivilegeController.java
@@ -1,46 +1,103 @@
 package controllers.yh;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import models.Privilege;
 import models.Role;
 import models.UserLogin;
 
 import com.jfinal.core.Controller;
+import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 
-
 public class PrivilegeController extends Controller {
-	public void index(){
-		if(LoginUserController.isAuthenticated(this))
-		render("profile/privilege/PrivilegeList.html");
+	private Logger logger = Logger.getLogger(PrivilegeController.class);
+
+	public void index() {
+		if (LoginUserController.isAuthenticated(this))
+			render("profile/privilege/PrivilegeList.html");
 	}
-	
-	public void userrole(){
-		if(LoginUserController.isAuthenticated(this))
-		render("profile/privilege/UserRole.html");
+
+	public void privilegelist() {
+		String sLimit = "";
+		String pageIndex = getPara("sEcho");
+		if (getPara("iDisplayStart") != null
+		        && getPara("iDisplayLength") != null) {
+			sLimit = " LIMIT " + getPara("iDisplayStart") + ", "
+			        + getPara("iDisplayLength");
+		}
+
+		// 获取总条数
+		String totalWhere = "";
+		String sql = "select count(1) total FROM MODULES ";
+		Record rec = Db.findFirst(sql + totalWhere);
+		logger.debug("total records:" + rec.getLong("total"));
+
+		String sql_m = "SELECT * FROM MODULES  " + sLimit;
+
+		// 获取当前页的数据
+		List<Record> orders = Db.find(sql_m);
+		for (int i = 0; i < orders.size(); i++) {
+
+			String sql_mp = "SELECT m.* ,p.* FROM MODULES_PRIVILEGE   mp left join MODULES  m on m.id=mp.MODULE_ID left join PRIVILEGES  p on p.id=mp.PRIVILEGE_ID where mp.module_id='"
+			        + orders.get(i).get("id") + "' ";
+			// 获取模块对应的权限
+			List<Record> module_p = Db.find(sql_mp);
+			orders.get(i).set("module_p", module_p);
+
+			// for (int j = 0; j < module_p.size(); j++) {
+			// String m_pr = module_p.get(j).get("privilege");
+			// orders.get(i).set("m_p" + j + " ", m_pr);
+			// }
+		}
+
+		Map orderMap = new HashMap();
+		orderMap.put("sEcho", pageIndex);
+		orderMap.put("iTotalRecords", rec.getLong("total"));
+		orderMap.put("iTotalDisplayRecords", rec.getLong("total"));
+
+		orderMap.put("aaData", orders);
+
+		renderJson(orderMap);
+
 	}
-	public void roleprivilege(){
-		if(LoginUserController.isAuthenticated(this))
-		render("profile/privilege/RolePrivilege.html");
+
+	public void editModule_pri() {
+		String[] m_p_list = getParaValues("localArr");
+		System.out.print(m_p_list);
 	}
-	public void SelectUser(){
-		String select=getPara("select");
-		
-		if(select.equals("1")){
-			List<UserLogin> userjson = UserLogin.dao.find("select * from user_login");			
-	        renderJson(userjson);
+
+	public void userrole() {
+		if (LoginUserController.isAuthenticated(this))
+			render("profile/privilege/UserRole.html");
+	}
+
+	public void roleprivilege() {
+		if (LoginUserController.isAuthenticated(this))
+			render("profile/privilege/RolePrivilege.html");
+	}
+
+	public void SelectUser() {
+		String select = getPara("select");
+
+		if (select.equals("1")) {
+			List<UserLogin> userjson = UserLogin.dao
+			        .find("select * from user_login");
+			renderJson(userjson);
 		}
-		
-		if(select.equals("2")){
-			List<Role> rolejson = Role.dao.find("select * from role_table");			
-	        renderJson(rolejson);
+
+		if (select.equals("2")) {
+			List<Role> rolejson = Role.dao.find("select * from role");
+			renderJson(rolejson);
 		}
-		if(select.equals("3")){
-			List<Privilege> rolejson = Privilege.dao.find("select * from privilege_table");			
-	        renderJson(rolejson);
+		if (select.equals("3")) {
+			List<Privilege> rolejson = Privilege.dao
+			        .find("select * from privilege_table");
+			renderJson(rolejson);
 		}
-		
+
 	}
 }
