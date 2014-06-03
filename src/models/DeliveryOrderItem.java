e2d4c85b12d09db17c8b908c336da52ae7d4376b
jam <237116624@qq.com>
2014-06-03 15:14:03 +0800

    bug

---
+++ src/models/DeliveryOrderItem.java
@@ -0,0 +1,7 @@
+package models;
+
+import com.jfinal.plugin.activerecord.Model;
+@SuppressWarnings("serial")
+public class DeliveryOrderItem extends Model<DeliveryOrderItem> {
+	public static final DeliveryOrderItem dao = new DeliveryOrderItem();
+}
