56cbf61476a46f3074495fce75f9e4eab5ef6523
xavier <643751802@qq.com>
2014-06-18 21:14:52 +0800

    添加departitemdetai

---
+++ src/models/DepartOrderItemdetail.java
@@ -0,0 +1,8 @@
+package models;
+
+import com.jfinal.plugin.activerecord.Model;
+@SuppressWarnings("serial")
+public class DepartOrderItemdetail extends Model<DepartOrderItemdetail> {
+	
+	public static final DepartOrderItemdetail dao = new DepartOrderItemdetail();
+}
