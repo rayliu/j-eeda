d7a7709fb0e17347d88cf378f87f7119d6f6af38
rayliu <liuyubie@gmail.com>
2014-06-21 17:49:49 +0800

    “产品”树的结构整理，只完成了添加功能

--- WebRoot/yh/js/profile/product/list.js
+++ WebRoot/yh/js/profile/product/list.js
@@ -35,8 +35,19 @@ $(document).ready(function() {
 		$('#customerMessage').val(message.substring(0, message.indexOf(" ")));
 		var customerId = $(this).attr('partyId');
 		$('#customerId').val(customerId);
+        var pageCustomerName = $("#pageCustomerName");
+
+
+        $.get('/yh/product/searchCustomerCategory', {customerId: customerId}, function(data){
+            console.log(data);
+            var zNodes =[{name: $("#customerMessage").val(), categoryId:data.ID, customerId: customerId, isParent:true}];
+            $.fn.zTree.init($("#categoryTree"), setting, zNodes);
+        },'json');
+
+       
+
 		$('#hiddenCustomerId').val($(this).attr('partyId'));
-		var pageCustomerName = $("#pageCustomerName");
+		
 		pageCustomerName.empty();
 		pageCustomerName.append($(this).attr('contact_person')+'&nbsp;');
 		pageCustomerName.append($(this).attr('phone')); 
@@ -49,28 +60,14 @@ $(document).ready(function() {
         $("#parentTree").text($("#customerMessage").val());
         
         // 查出customer的子类别
-        if(customerId > 0){
-    	    $.get('/yh/product/searchCustomerCategory', {customerId:customerId}, function(data){
-    			console.log(data);	
-    			var productTreeUl = $("#productTreeUl");
-    			productTreeUl.empty();
-    			addNode(data, productTreeUl);
-//    			if(data.length > 0){
-//    				for(var i = 0; i < data.length; i++)
-//    				{
-//    					var id = data[i].ID;
-//    					if(data[i].PARENT_ID == null){
-//	    					productTreeUl.append("<li class=' parent_li productList' categoryId='"+data[i].ID+"'><span ><i class='fa fa-folder'>" +
-//	    							"</i>  <a href='javascript:void(0)' id='"+data[i].ID+"'>"+data[i].NAME+
-//	    							"</a></span>  <div class='edit_icons'><a class='fa fa-plus addCategory' href='javascript:void(0)' title='添加' id='"+data[i].ID+"'></a>  " +
-//	    							"<a class='fa fa-pencil editCategory' href='javascript:void(0)' title='编辑' id='"+data[i].ID+"'></a>  " +
-//	    							"<a class='fa fa-trash-o deleteCategory' href='javascript:void(0)' title='删除' id='"+data[i].ID+"'></a></div></li></ul></li>");
-//
-//    					}
-//    				}
-//    			}
-    		},'json');
-        }
+      //   if(customerId > 0){
+    	 //    $.get('/yh/product/searchCustomerCategory', {customerId:customerId}, function(data){
+    		// 	console.log(data);	
+    		// 	var productTreeUl = $("#productTreeUl");
+    		// 	productTreeUl.empty();
+    		// 	addNode(data, productTreeUl);
+    		// },'json');
+      //   }
         $("#addProductDiv").show();
         $("#displayDiv").hide();
     }); 
@@ -354,4 +351,196 @@ $(document).ready(function() {
              }
 		},'json');    	
     });
+
+
+    //---------------tree handle
+    var setting = {
+            view: {
+                addHoverDom: addHoverDom,
+                removeHoverDom: removeHoverDom,
+                selectedMulti: false
+            },
+            edit: {
+                enable: true,
+                editNameSelectAll: true,
+                showRemoveBtn: showRemoveBtn,
+                showRenameBtn: showRenameBtn
+            },
+            async: {
+                enable: true,
+                url:"/yh/product/searchNodeCategory",
+                autoParam:["categoryId", "customerId", "level=lv"],
+                otherParam:{"otherParam":"zTreeAsyncTest"},
+                dataFilter: filter
+            },
+            callback: {
+                beforeClick: beforeClick,
+                onClick: onNodeClick,
+                beforeAsync: beforeAsync,
+                onAsyncError: onAsyncError,
+                onAsyncSuccess: onAsyncSuccess,
+
+                beforeDrag: beforeDrag,
+                beforeEditName: beforeEditName,
+                beforeRemove: beforeRemove,
+                beforeRename: beforeRename,
+                onRemove: onRemove,
+                onRename: onRename
+            }
+        };
+        
+        //-------------edit--------------
+        var log, className = "dark";
+        function beforeDrag(treeId, treeNodes) {
+            return false;
+        }
+
+        function beforeEditName(treeId, treeNode) {
+            className = (className === "dark" ? "":"dark");
+            showLog("[ "+getTime()+" beforeEditName ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name);
+            var zTree = $.fn.zTree.getZTreeObj("categoryTree");
+            zTree.selectNode(treeNode);
+            return confirm("进入节点 -- " + treeNode.name + " 的编辑状态吗？");
+        }
+        function beforeRemove(treeId, treeNode) {
+            className = (className === "dark" ? "":"dark");
+            showLog("[ "+getTime()+" beforeRemove ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name);
+            var zTree = $.fn.zTree.getZTreeObj("categoryTree");
+            zTree.selectNode(treeNode);
+            return confirm("确认删除 节点 -- " + treeNode.name + " 吗？");
+        }
+        function onRemove(e, treeId, treeNode) {
+            showLog("[ "+getTime()+" onRemove ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name);
+        }
+        function beforeRename(treeId, treeNode, newName, isCancel) {
+            className = (className === "dark" ? "":"dark");
+            showLog((isCancel ? "<span style='color:red'>":"") + "[ "+getTime()+" beforeRename ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name + (isCancel ? "</span>":""));
+            if (newName.length == 0) {
+                alert("节点名称不能为空.");
+                var zTree = $.fn.zTree.getZTreeObj("categoryTree");
+                setTimeout(function(){zTree.editName(treeNode)}, 10);
+                return false;
+            }
+            return true;
+        }
+        function onRename(e, treeId, treeNode, isCancel) {
+            showLog((isCancel ? "<span style='color:red'>":"") + "[ "+getTime()+" onRename ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name + (isCancel ? "</span>":""));
+        }
+        function showRemoveBtn(treeId, treeNode) {
+            //根节点，不能删除
+            if(treeNode.level==0)
+                return false;
+            //有子节点，不能删除
+            return true;
+            
+        }
+
+        function showRenameBtn(treeId, treeNode) {
+            //根节点，不能重命名
+            if(treeNode.level==0)
+                return false;
+            
+            return true;
+        }
+
+        var newCount = 1;
+        function addHoverDom(treeId, treeNode) {
+            var sObj = $("#" + treeNode.tId + "_span");
+            if (treeNode.editNameFlag || $("#addBtn_"+treeNode.tId).length>0) return;
+            var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
+                + "' title='添加类别' onfocus='this.blur();'></span>";
+            sObj.after(addStr);
+            var btn = $("#addBtn_"+treeNode.tId);
+            if (btn) btn.bind("click", function(){
+                //异步创建节点
+                var zTree = $.fn.zTree.getZTreeObj("categoryTree");
+                var nodeName = "新类别" + (newCount++);                
+
+                $.post('/yh/product/saveCategory', {parentId: treeNode.categoryId, customerId: treeNode.customerId, name:nodeName}, function(data){            
+                    zTree.addNodes(treeNode, {id:111111, categoryId: data.ID, customerId: treeNode.customerId, isParent:true, name:nodeName});
+                },'json');
+
+                
+                return false;
+            });
+        };
+        function removeHoverDom(treeId, treeNode) {
+            $("#addBtn_"+treeNode.tId).unbind().remove();
+        };
+        function selectAll() {
+            var zTree = $.fn.zTree.getZTreeObj("categoryTree");
+            zTree.setting.edit.editNameSelectAll =  $("#selectAll").attr("checked");
+        }
+        //-------------edit end--------------
+        function onNodeClick(event, treeId, treeNode){          
+            $("#displayDiv").show();
+            productDataTable.fnSettings().sAjaxSource = "/yh/product/list?categoryId="+treeNode.categoryId;
+            productDataTable.fnDraw();
+        }
+
+        function filter(treeId, parentNode, childNodes) {
+            if (!childNodes) return null;
+            for (var i=0, l=childNodes.length; i<l; i++) {
+                childNodes[i].name = childNodes[i].NAME.replace(/\.n/g, '.');
+                childNodes[i].categoryId = childNodes[i].ID;
+                childNodes[i].customerId = childNodes[i].CUSTOMER_ID;
+                childNodes[i].parentId = childNodes[i].PARENT_ID;
+                childNodes[i].isParent=true;
+            }
+            return childNodes;
+        }
+        function beforeClick(treeId, treeNode) {
+            if (!treeNode.isParent) {
+                alert("pls select parent node");
+                return false;
+            } else {
+                return true;
+            }
+        }
+        var log, className = "dark";
+        function beforeAsync(treeId, treeNode) {
+            className = (className === "dark" ? "":"dark");
+            showLog("[ "+getTime()+" beforeAsync ]&nbsp;&nbsp;&nbsp;&nbsp;" + ((!!treeNode && !!treeNode.name) ? treeNode.name : "root") );
+            return true;
+        }
+        function onAsyncError(event, treeId, treeNode, XMLHttpRequest, textStatus, errorThrown) {
+            showLog("[ "+getTime()+" onAsyncError ]&nbsp;&nbsp;&nbsp;&nbsp;" + ((!!treeNode && !!treeNode.name) ? treeNode.name : "root") );
+        }
+        function onAsyncSuccess(event, treeId, treeNode, msg) {
+            showLog("[ "+getTime()+" onAsyncSuccess ]&nbsp;&nbsp;&nbsp;&nbsp;" + ((!!treeNode && !!treeNode.name) ? treeNode.name : "root") );
+        }
+        
+        function showLog(str) {
+            if (!log) log = $("#log");
+            log.append("<li class='"+className+"'>"+str+"</li>");
+            if(log.children("li").length > 8) {
+                log.get(0).removeChild(log.children("li")[0]);
+            }
+        }
+        function getTime() {
+            var now= new Date(),
+            h=now.getHours(),
+            m=now.getMinutes(),
+            s=now.getSeconds(),
+            ms=now.getMilliseconds();
+            return (h+":"+m+":"+s+ " " +ms);
+        }
+
+        function refreshNode(e) {
+            var zTree = $.fn.zTree.getZTreeObj("categoryTree"),
+            type = e.data.type,
+            silent = e.data.silent,
+            nodes = zTree.getSelectedNodes();
+            if (nodes.length == 0) {
+                alert("pls select parent node");
+            }
+            for (var i=0, l=nodes.length; i<l; i++) {
+                zTree.reAsyncChildNodes(nodes[i], type, silent);
+                if (!silent) zTree.selectNode(nodes[i]);
+            }
+        }
+
+        var zNodes =[{}];
+
+        $.fn.zTree.init($("#categoryTree"), setting, zNodes);
 } );
--- src/controllers/yh/profile/ProductController.java
+++ src/controllers/yh/profile/ProductController.java
@@ -15,230 +15,229 @@ import com.jfinal.plugin.activerecord.Record;
 
 import controllers.yh.LoginUserController;
 
-public class ProductController extends Controller{
+public class ProductController extends Controller {
 
     private Logger logger = Logger.getLogger(ProductController.class);
-    
-	public void index() {
-		if(LoginUserController.isAuthenticated(this))
-		render("profile/product/productList.html");
-	}
 
-	public void list() {
-		String sLimit = "";
-		Map productListMap = null;
-		String categoryId = getPara("categoryId");
-		if(categoryId == null || "".equals(categoryId)){
-			String pageIndex = getPara("sEcho");
-			if (getPara("iDisplayStart") != null
-					&& getPara("iDisplayLength") != null) {
-				sLimit = " LIMIT " + getPara("iDisplayStart") + ", "
-						+ getPara("iDisplayLength");
-			}
-	
-			String category = getPara("category");
-			String sqlTotal = "select count(1) total from product";
-			Record rec = Db.findFirst(sqlTotal);
-			logger.debug("total records:" + rec.getLong("total"));
-	
-			String sql = "select * from product";
-	
-			List<Record> products = Db.find(sql);
-	
-			productListMap = new HashMap();
-			productListMap.put("sEcho", pageIndex);
-			productListMap.put("iTotalRecords", rec.getLong("total"));
-			productListMap.put("iTotalDisplayRecords", rec.getLong("total"));
-	
-			productListMap.put("aaData", products);
-		}else{
-			String pageIndex = getPara("sEcho");
-			if (getPara("iDisplayStart") != null
-					&& getPara("iDisplayLength") != null) {
-				sLimit = " LIMIT " + getPara("iDisplayStart") + ", "
-						+ getPara("iDisplayLength");
-			}
-	
-			String category = getPara("category");
-			String sqlTotal = "select count(1) total from product where category_id = " + categoryId;
-			Record rec = Db.findFirst(sqlTotal);
-			logger.debug("total records:" + rec.getLong("total"));
-	
-			String sql = "select * from product where category_id = " + categoryId;
-	
-			List<Record> products = Db.find(sql);
-	
-			productListMap = new HashMap();
-			productListMap.put("sEcho", pageIndex);
-			productListMap.put("iTotalRecords", rec.getLong("total"));
-			productListMap.put("iTotalDisplayRecords", rec.getLong("total"));
-	
-			productListMap.put("aaData", products);
-		}
-		renderJson(productListMap);
-	}
+    public void index() {
+        if (LoginUserController.isAuthenticated(this))
+            render("profile/product/productList.html");
+    }
 
-	public void add() {
-		setAttr("saveOK", false);
-		if(LoginUserController.isAuthenticated(this))
-		render("profile/product/productEdit.html");
-	}
+    public void list() {
+        String sLimit = "";
+        Map productListMap = null;
+        String categoryId = getPara("categoryId");
+        if (categoryId == null || "".equals(categoryId)) {
+            String pageIndex = getPara("sEcho");
+            if (getPara("iDisplayStart") != null && getPara("iDisplayLength") != null) {
+                sLimit = " LIMIT " + getPara("iDisplayStart") + ", " + getPara("iDisplayLength");
+            }
 
-	public void edit() {
-		long id = getParaToLong();
+            String category = getPara("category");
+            String sqlTotal = "select count(1) total from product";
+            Record rec = Db.findFirst(sqlTotal);
+            logger.debug("total records:" + rec.getLong("total"));
 
-		Product product = Product.dao.findById(id);
-		setAttr("product", product);
-		if(LoginUserController.isAuthenticated(this))
-		render("profile/product/productEdit.html");
-	}
+            String sql = "select * from product";
 
-	public void delete() {
-		Product product = Product.dao.findById(getPara("productId"));
-		product.set("category_id", null);
-		product.delete();
+            List<Record> products = Db.find(sql);
+
+            productListMap = new HashMap();
+            productListMap.put("sEcho", pageIndex);
+            productListMap.put("iTotalRecords", rec.getLong("total"));
+            productListMap.put("iTotalDisplayRecords", rec.getLong("total"));
+
+            productListMap.put("aaData", products);
+        } else {
+            String pageIndex = getPara("sEcho");
+            if (getPara("iDisplayStart") != null && getPara("iDisplayLength") != null) {
+                sLimit = " LIMIT " + getPara("iDisplayStart") + ", " + getPara("iDisplayLength");
+            }
+
+            String category = getPara("category");
+            String sqlTotal = "select count(1) total from product where category_id = " + categoryId;
+            Record rec = Db.findFirst(sqlTotal);
+            logger.debug("total records:" + rec.getLong("total"));
+
+            String sql = "select * from product where category_id = " + categoryId;
+
+            List<Record> products = Db.find(sql);
+
+            productListMap = new HashMap();
+            productListMap.put("sEcho", pageIndex);
+            productListMap.put("iTotalRecords", rec.getLong("total"));
+            productListMap.put("iTotalDisplayRecords", rec.getLong("total"));
+
+            productListMap.put("aaData", products);
+        }
+        renderJson(productListMap);
+    }
+
+    public void add() {
+        setAttr("saveOK", false);
+        if (LoginUserController.isAuthenticated(this))
+            render("profile/product/productEdit.html");
+    }
+
+    public void edit() {
+        long id = getParaToLong();
+
+        Product product = Product.dao.findById(id);
+        setAttr("product", product);
+        if (LoginUserController.isAuthenticated(this))
+            render("profile/product/productEdit.html");
+    }
+
+    public void delete() {
+        Product product = Product.dao.findById(getPara("productId"));
+        product.set("category_id", null);
+        product.delete();
         renderJson("{\"success\":true}");
-	}
+    }
 
-	public void save() {
-		Product product = null;
-		String id = getPara("id");
-		
-		if (id != null && !id.equals("")) {
-			product = Product.dao.findById(id);
-			String size = getPara("size");
-			String width = getPara("width");
-			String volume = getPara("volume");
-			String weight = getPara("weight");
-			product.set("item_name", getPara("item_name"))
-			       .set("item_no", getPara("item_no"))
-	               .set("item_desc", getPara("item_desc"))
-				   .set("unit", getPara("unit"))				   
-				   .set("category_id", getPara("categorySelect"));
-			if(size != null && !"".equals(size)){
-				product.set("size", size);
-			}
-			if(width != null && !"".equals(weight)){
-				product.set("width", width);
-			}
-			if(volume != null && !"".equals(volume)){
-				product.set("volume", volume);
-			}
-			if(weight != null && !"".equals(weight)){
-				product.set("weight", weight);
-			}
-	        product.update();
-		} else {
-			product = new Product();
-			String itemName = getPara("item_name");
-			String itemNo = getPara("item_no");
-			String itemDesc = getPara("item_desc");
-			String size = getPara("size");
-			String width = getPara("width");
-			String unit = getPara("unit");
-			String volume = getPara("volume");
-			String weight = getPara("weight");
+    public void save() {
+        Product product = null;
+        String id = getPara("id");
 
-			product.set("item_name", itemName)
-				   .set("item_no", itemNo)
-			       .set("item_desc", itemDesc)
-			       .set("unit", getPara("unit"))
-				   .set("category_id", getPara("categorySelect"));
-			if(size != null && !"".equals(size)){
-				product.set("size", size);
-			}
-			if(width != null && !"".equals(weight)){
-				product.set("width", width);
-			}
-			if(volume != null && !"".equals(volume)){
-				product.set("volume", volume);
-			}
-			if(weight != null && !"".equals(weight)){
-				product.set("weight", weight);
-			}
-	        product.save();
-		}
-		renderJson(product);;
-	}
-	
-	// 查出客户的子类别
-	public void searchCustomerCategory(){
-		String customerId = getPara("customerId");
-		List<Category> categories = Category.dao.find("select * from category where customer_id ="+customerId);
-		renderJson(categories);
-	}
-	
-	//查出当前节点的子节点
-	public void searchNodeCategory(){
-		String categoryId = getPara("categoryId");
-		String customerId = getPara("customerId");
-		List<Category> categories = Category.dao.find("select * from category where parent_id="+categoryId+" and customer_id ="+customerId);
-		renderJson(categories);
-	}
-	
-	// 查找产品对象
-	public void getProduct(){
-		Product product = Product.dao.findById(getPara("productId"));
-		renderJson(product);
-	}
-	
-	// 保存类别
-	public void saveCategory(){
-		String categoryId = getPara("categoryId");
-		String parentId = getPara("parentId");
-		Category category = null;
-		if(categoryId == null || "".equals(categoryId)){
-			category = new Category();
-			category.set("name", getPara("name"));
-			category.set("customer_id", getPara("customerId"));
-			if(parentId != null && !"".equals(parentId)){
-				category.set("parent_id", getPara("parentId"));				
-			}
-			category.save();
-		}else{
-			category = Category.dao.findById(categoryId);
-			category.set("name", getPara("name"));
-			category.set("customer_id", getPara("customerId"));
-			category.update();
-		}
-		renderJson(category);
-	}
-	
-	// 删除类别
-	public void deleteCategory(){
-		String cid = getPara("categoryId");
-		removeChildern(cid);
-		List<Product> products = Product.dao.find("select * from product where category_id = ?", cid);	
-		for(Product product : products){
-			product.delete();
-		}
-		Category.dao.deleteById(cid);
+        if (id != null && !id.equals("")) {
+            product = Product.dao.findById(id);
+            String size = getPara("size");
+            String width = getPara("width");
+            String volume = getPara("volume");
+            String weight = getPara("weight");
+            product.set("item_name", getPara("item_name")).set("item_no", getPara("item_no")).set("item_desc", getPara("item_desc"))
+                    .set("unit", getPara("unit")).set("category_id", getPara("categorySelect"));
+            if (size != null && !"".equals(size)) {
+                product.set("size", size);
+            }
+            if (width != null && !"".equals(weight)) {
+                product.set("width", width);
+            }
+            if (volume != null && !"".equals(volume)) {
+                product.set("volume", volume);
+            }
+            if (weight != null && !"".equals(weight)) {
+                product.set("weight", weight);
+            }
+            product.update();
+        } else {
+            product = new Product();
+            String itemName = getPara("item_name");
+            String itemNo = getPara("item_no");
+            String itemDesc = getPara("item_desc");
+            String size = getPara("size");
+            String width = getPara("width");
+            String unit = getPara("unit");
+            String volume = getPara("volume");
+            String weight = getPara("weight");
+
+            product.set("item_name", itemName).set("item_no", itemNo).set("item_desc", itemDesc).set("unit", getPara("unit"))
+                    .set("category_id", getPara("categorySelect"));
+            if (size != null && !"".equals(size)) {
+                product.set("size", size);
+            }
+            if (width != null && !"".equals(weight)) {
+                product.set("width", width);
+            }
+            if (volume != null && !"".equals(volume)) {
+                product.set("volume", volume);
+            }
+            if (weight != null && !"".equals(weight)) {
+                product.set("weight", weight);
+            }
+            product.save();
+        }
+        renderJson(product);
+        ;
+    }
+
+    // 查出客户的根类别
+    public void searchCustomerCategory() {
+        String customerId = getPara("customerId");
+        Category category = Category.dao.findFirst("select * from category where customer_id =? and parent_id is null", customerId);
+        if (category == null) {
+            category = new Category();
+            category.set("name", "root");
+            category.set("customer_id", customerId);
+            category.save();
+        }
+
+        renderJson(category);
+    }
+
+    // 查出当前节点的子节点
+    public void searchNodeCategory() {
+        String categoryId = getPara("categoryId");
+        String customerId = getPara("customerId");
+        List<Category> categories = Category.dao.find("select * from category where parent_id=" + categoryId + " and customer_id ="
+                + customerId);
+        renderJson(categories);
+    }
+
+    // 查找产品对象
+    public void getProduct() {
+        Product product = Product.dao.findById(getPara("productId"));
+        renderJson(product);
+    }
+
+    // 保存类别
+    public void saveCategory() {
+        String categoryId = getPara("categoryId");
+        String parentId = getPara("parentId");
+        Category category = null;
+        if (categoryId == null || "".equals(categoryId)) {
+            category = new Category();
+            category.set("name", getPara("name"));
+            category.set("customer_id", getPara("customerId"));
+            if (parentId != null && !"".equals(parentId)) {
+                category.set("parent_id", getPara("parentId"));
+            }
+            category.save();
+        } else {
+            category = Category.dao.findById(categoryId);
+            category.set("name", getPara("name"));
+            category.set("customer_id", getPara("customerId"));
+            category.update();
+        }
+        renderJson(category);
+    }
+
+    // 删除类别
+    public void deleteCategory() {
+        String cid = getPara("categoryId");
+        removeChildern(cid);
+        List<Product> products = Product.dao.find("select * from product where category_id = ?", cid);
+        for (Product product : products) {
+            product.delete();
+        }
+        Category.dao.deleteById(cid);
         renderJson("{\"success\":true}");
-	}	
-	
-	private void removeChildern(String cid) {
-		List<Category> categories = Category.dao.find("select * from category where parent_id = ?", cid);
-		if(categories.size() > 0){
-			for(Category c : categories){
-				removeChildern(c.get("id").toString());
-				List<Product> products = Product.dao.find("select * from product where category_id = ?", c.get("id"));	
-				for(Product product : products){
-					product.delete();
-				}
-				c.delete();
-			}
-		}else{
-			List<Product> products = Product.dao.find("select * from product where category_id = ?", cid);	
-			for(Product product : products){
-				product.delete();
-			}
-			Category.dao.deleteById(cid);
-		}
-	}
+    }
 
-	// 查找类别
-	public void searchCategory(){
-		Category category = Category.dao.findById(getPara("categoryId"));
-		renderJson(category);
-	}
+    private void removeChildern(String cid) {
+        List<Category> categories = Category.dao.find("select * from category where parent_id = ?", cid);
+        if (categories.size() > 0) {
+            for (Category c : categories) {
+                removeChildern(c.get("id").toString());
+                List<Product> products = Product.dao.find("select * from product where category_id = ?", c.get("id"));
+                for (Product product : products) {
+                    product.delete();
+                }
+                c.delete();
+            }
+        } else {
+            List<Product> products = Product.dao.find("select * from product where category_id = ?", cid);
+            for (Product product : products) {
+                product.delete();
+            }
+            Category.dao.deleteById(cid);
+        }
+    }
+
+    // 查找类别
+    public void searchCategory() {
+        Category category = Category.dao.findById(getPara("categoryId"));
+        renderJson(category);
+    }
 }
