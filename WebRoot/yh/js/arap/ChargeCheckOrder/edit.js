2152490d2244f2788b59d5f4cd31c4a8c4c95ce0
speculate <12476529342@qq.com>
2014-09-03 09:44:22 +0800

    分离出应收对账单的js

---
+++ WebRoot/yh/js/arap/ChargeCheckOrder/edit.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+	$('#menu_charge').addClass('active').find('ul').addClass('in');
+	
+	var saveChargeCheckOrder = function(e){
+		//阻止a 的默认响应行为，不需要跳转
+		e.preventDefault();
+		//提交前，校验数据
+        if(!$("#chargeCheckOrderForm").valid()){
+	       	return;
+        }
+		//异步向后台提交数据
+		$.post('/yh/chargeCheckOrder/save', $("#chargeCheckOrderForm").serialize(), function(data){
+			if(data.ID>0){
+				$("#chargeCheckOrderId").val(data.ID);
+			  	$("#style").show();
+			  	$("#departureConfirmationBtn").attr("disabled", false);
+			}else{
+				alert('数据保存失败。');
+			}
+		},'json');
+	};
+    
+	//点击保存的事件，保存运输单信息
+	//transferOrderForm 不需要提交	
+ 	$("#saveChargeCheckOrderBtn").click(function(e){
+ 		saveChargeCheckOrder(e);
+	});
+	
+	$("#chargeCheckOrderItem").click(function(e){
+		//阻止a 的默认响应行为，不需要跳转
+		e.preventDefault();
+		//提交前，校验数据
+        if(!$("#chargeCheckOrderForm").valid()){
+	       	return;
+        }
+		//异步向后台提交数据
+		$.post('/yh/chargeCheckOrder/save', $("#chargeCheckOrderForm").serialize(), function(data){
+			if(data.ID>0){
+				$("#chargeCheckOrderId").val(data.ID);
+			  	$("#style").show();
+			  	$("#departureConfirmationBtn").attr("disabled", false);
+	
+			    var chargeCheckOrderId = $("#chargeCheckOrderId").val();
+				returnOrderTable.fnSettings().sAjaxSource = "/yh/chargeCheckOrder/returnOrderList?chargeCheckOrderId="+chargeCheckOrderId;
+				returnOrderTable.fnDraw(); 
+			}else{
+				alert('数据保存失败。');
+			}
+		},'json');
+	});
+	
+    if($("#chargeCheckOrderStatus").text() == 'new'){
+    	$("#chargeCheckOrderStatus").text('新建');
+	}
+
+	var returnOrderTable =$('#example').dataTable( {
+	    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+        //"sPaginationType": "bootstrap",insert into return_order(status_code,create_date,order_type,creator,remark,transfer_order,distribution_order_id,contract_id
+        "iDisplayLength": 10,
+        "bServerSide": true,
+    	"oLanguage": {
+            "sUrl": "/eeda/dataTables.ch.txt"
+        },
+        "sAjaxSource": "/yh/chargeCheckOrder/returnOrderList",
+   			"aoColumns": [
+   			{ "mDataProp": "ORDER_NO",
+            	"fnRender": function(obj) {
+        			return "<a href='/yh/returnOrder/edit?id="+obj.aData.ID+"'>"+obj.aData.ORDER_NO+"</a>";
+        		}},
+            { "mDataProp": "CNAME"},
+            { "mDataProp": "TRANSFER_ORDER_NO"},
+            { "mDataProp": "DELIVERY_ORDER_NO"},
+            { "mDataProp": "CREATOR_NAME" },
+            { "mDataProp": "CREATE_DATE" },
+            { "mDataProp": "TRANSACTION_STATUS",
+                "fnRender": function(obj) {
+                    if(obj.aData.TRANSACTION_STATUS=='new')
+                        return '新建';
+                    if(obj.aData.TRANSACTION_STATUS=='confirmed')
+                        return '已确认';
+                    if(obj.aData.TRANSACTION_STATUS=='cancel')
+                        return '取消';
+                    
+                    return obj.aData.TRANSACTION_STATUS;
+                 }
+            },
+            { "mDataProp": "REMARK" },
+            { "mDataProp": "AMOUNT"	},
+            { "mDataProp": null	}
+         ]
+	});
+} );
