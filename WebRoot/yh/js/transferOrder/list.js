a574cc62cd4ad6f4238d6818322a497cfb8270d2
redluo <red.luo@eeda123.com>
2014-09-12 11:21:27 +0800

    运输单客户供应商可选可输，有选项框

--- WebRoot/yh/js/transferOrder/list.js
+++ WebRoot/yh/js/transferOrder/list.js
@@ -153,7 +153,152 @@ $(document).ready(function() {
     	transferOrder.fnSettings().sAjaxSource = "/yh/transferOrder/list?orderNo="+orderNo+"&status="+status+"&address="+address+"&customer="+customer+"&sp="+sp+"&beginTime="+beginTime+"&endTime="+endTime+"&officeName="+officeName;
     	transferOrder.fnDraw();
     } );
+    //获取客户列表，自动填充
+    $('#customer_filter').on('keyup click', function(){
+        var inputStr = $('#customer_filter').val();
+        
+        $.get("/yh/customerContract/search", {locationName:inputStr}, function(data){
+            console.log(data);
+            var companyList =$("#companyList");
+            companyList.empty();
+            for(var i = 0; i < data.length; i++)
+            {
+                companyList.append("<li><a tabindex='-1' class='fromLocationItem' post_code='"+data[i].POSTAL_CODE+"' contact_person='"+data[i].CONTACT_PERSON+"' email='"+data[i].EMAIL+"' phone='"+data[i].PHONE+"' partyId='"+data[i].PID+"' address='"+data[i].ADDRESS+"', company_name='"+data[i].COMPANY_NAME+"', >"+data[i].ABBR+"</a></li>");
+            }
+            if(data.length>0)
+                companyList.show();
+            
+        },'json');
+
+        if(inputStr==''){
+        	transferOrder.fnFilter('', 2);
+        }
+        
+    });
+    $('#companyList').on('click', '.fromLocationItem', function(e){        
+        $('#customer_filter').val($(this).text());
+        $("#companyList").hide();
+        var companyId = $(this).attr('partyId');
+        $('#customerId').val(companyId);
+        //过滤回单列表
+        //chargeCheckTable.fnFilter(companyId, 2);
+        var orderNo = $("#orderNo_filter").val();
+    	var status = $("#status_filter").val();
+    	var address = $("#address_filter").val();
+    	var customer = $("#customer_filter").val();
+    	var sp = $("#sp_filter").val();
+    	var beginTime = $("#beginTime_filter").val();
+    	var endTime = $("#endTime_filter").val();
+    	var officeName = $("#officeName_filter").val();
+        if(customer!=null){
+        	transferOrder.fnSettings().sAjaxSource = "/yh/transferOrder/list?orderNo="+orderNo+"&status="+status+"&address="+address+"&customer="+customer+"&sp="+sp+"&beginTime="+beginTime+"&endTime="+endTime+"&officeName="+officeName;
+        	transferOrder.fnDraw();
+        }
+    });
+    // 没选中客户，焦点离开，隐藏列表
+    $('#customer_filter').on('blur', function(){
+        $('#companyList').hide();
+    });
+
+    //当用户只点击了滚动条，没选客户，再点击页面别的地方时，隐藏列表
+    $('#customer_filter').on('blur', function(){
+        $('#companyList').hide();
+    });
+
+    $('#companyList').on('mousedown', function(){
+        return false;//阻止事件回流，不触发 $('#spMessage').on('blur'
+    });
+
+    //供应商查询
+  //获取供应商的list，选中信息在下方展示其他信息
+    $('#sp_filter').on('keyup click', function(){
+		var inputStr = $('#sp_filter').val();
+		$.get('/yh/transferOrder/searchSp', {input:inputStr}, function(data){
+			console.log(data);
+			var spList =$("#spList");
+			spList.empty();
+			for(var i = 0; i < data.length; i++)
+			{
+				var company_name = data[i].COMPANY_NAME;
+				if(company_name == null){
+					company_name = '';
+				}
+				var contact_person = data[i].CONTACT_PERSON;
+				if(contact_person == null){
+					contact_person = '';
+				}
+				var phone = data[i].PHONE;
+				if(phone == null){
+					phone = '';
+				}
+				spList.append("<li><a tabindex='-1' class='fromLocationItem' partyId='"+data[i].PID+"' post_code='"+data[i].POSTAL_CODE+"' contact_person='"+data[i].CONTACT_PERSON+"' email='"+data[i].EMAIL+"' phone='"+data[i].PHONE+"' spid='"+data[i].ID+"' address='"+data[i].ADDRESS+"', company_name='"+data[i].COMPANY_NAME+"', >"+company_name+" "+contact_person+" "+phone+"</a></li>");
+			}
+		},'json');
+
+		$("#spList").css({ 
+        	left:$(this).position().left+"px", 
+        	top:$(this).position().top+32+"px" 
+        }); 
+        $('#spList').show();
+    });
     
+ // 没选中供应商，焦点离开，隐藏列表
+	$('#sp_filter').on('blur', function(){
+ 		$('#spList').hide();
+ 	});
+
+	//当用户只点击了滚动条，没选供应商，再点击页面别的地方时，隐藏列表
+	$('#spList').on('blur', function(){
+ 		$('#spList').hide();
+ 	});
+
+	$('#spList').on('mousedown', function(){
+		return false;//阻止事件回流，不触发 $('#spMessage').on('blur'
+	});
+
+	// 选中供应商
+	$('#spList').on('mousedown', '.fromLocationItem', function(e){
+		console.log($('#spList').is(":focus"));
+		var message = $(this).text();
+		$('#sp_filter').val(message.substring(0, message.indexOf(" ")));
+		$('#sp_id').val($(this).attr('partyId'));
+		var pageSpName = $("#pageSpName");
+		pageSpName.empty();
+		var pageSpAddress = $("#pageSpAddress");
+		pageSpAddress.empty();
+		pageSpAddress.append($(this).attr('address'));
+		var contact_person = $(this).attr('contact_person');
+		if(contact_person == 'null'){
+			contact_person = '';
+		}
+		pageSpName.append(contact_person+'&nbsp;');
+		var phone = $(this).attr('phone');
+		if(phone == 'null'){
+			phone = '';
+		}
+		pageSpName.append(phone); 
+		pageSpAddress.empty();
+		var address = $(this).attr('address');
+		if(address == 'null'){
+			address = '';
+		}
+		pageSpAddress.append(address);
+        $('#spList').hide();
+        //根据选中的名称过滤
+        var orderNo = $("#orderNo_filter").val();
+    	var status = $("#status_filter").val();
+    	var address = $("#address_filter").val();
+    	var customer = $("#customer_filter").val();
+    	var sp = $("#sp_filter").val();
+    	var beginTime = $("#beginTime_filter").val();
+    	var endTime = $("#endTime_filter").val();
+    	var officeName = $("#officeName_filter").val();
+    	transferOrder.fnSettings().sAjaxSource = "/yh/transferOrder/list?orderNo="+orderNo+"&status="+status+"&address="+address+"&customer="+customer+"&sp="+sp+"&beginTime="+beginTime+"&endTime="+endTime+"&officeName="+officeName;
+    	transferOrder.fnDraw();
+    });
+    
+    
+    //根据输入的名称过滤
     $('input.customer_filter').on( 'keyup click', function () {
     	var orderNo = $("#orderNo_filter").val();
     	var status = $("#status_filter").val();
