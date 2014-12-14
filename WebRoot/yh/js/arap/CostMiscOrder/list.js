ae43b24d6906338931e17f960937e99587755244
Ray Liu <liuyubie@gmail.com>
2014-12-14 21:54:57 +0800

    手工付款单：UI+table，初步代码。

---
+++ WebRoot/yh/js/arap/CostMiscOrder/list.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+
+    $('#menu_finance').addClass('active').find('ul').addClass('in');
+
+	//datatable, 动态处理
+    var datatable=$('#chargeMiscOrderList-table').dataTable({
+        "bFilter": false, //不需要默认的搜索框
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r><'datatable-scroll't><'row-fluid'<'span12'i><'span12 center'p>>",
+        "iDisplayLength": 10,
+        "bServerSide": true,
+    	  "oLanguage": {
+            "sUrl": "/eeda/dataTables.ch.txt"
+        },
+        "sAjaxSource": "/chargeMiscOrder/list",
+        "aoColumns": [   
+            {"mDataProp":"ORDER_NO","sWidth": "80px",
+            	"fnRender": function(obj) {
+        			return "<a href='/chargeMiscOrder/edit?id="+obj.aData.ID+"'>"+obj.aData.ORDER_NO+"</a>";
+        		}},
+            {"mDataProp":"TYPE","sWidth": "100px",
+            	"fnRender": function(obj) {
+                    if(obj.aData.TYPE=='ordinary_receivables'){
+                        return '普通收款';
+                    }else if(obj.aData.TYPE=='offset_payment'){
+                        return '抵销货款';
+                    }
+                    return obj.aData.TYPE;
+                }
+            },
+            {"mDataProp":"STATUS","sWidth": "100px",
+                "fnRender": function(obj) {
+                    if(obj.aData.STATUS=='new'){
+                        return '新建';
+                    }else if(obj.aData.STATUS=='checking'){
+                        return '已发送对帐';
+                    }else if(obj.aData.STATUS=='confirmed'){
+                        return '已审核';
+                    }else if(obj.aData.STATUS=='completed'){
+                        return '已结算';
+                    }else if(obj.aData.STATUS=='cancel'){
+                        return '取消';
+                    }
+                    return obj.aData.STATUS;
+                }
+            },
+            {"mDataProp":"CREATE_STAMP","sWidth": "150px"},
+            {"mDataProp":"CHARGE_ORDER_NO","sWidth": "150px"},
+            {"mDataProp":"REMARK","sWidth": "150px"}                       
+        ]      
+    });	 
+} );
