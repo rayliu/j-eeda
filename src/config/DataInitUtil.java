dd14881713114c362c83eda25b848a1794ee5363
Ray Liu <liuyubie@gmail.com>
2014-12-21 11:29:22 +0800

    配送单：普通货品页面初始化。

--- src/config/DataInitUtil.java
+++ src/config/DataInitUtil.java
@@ -303,6 +303,7 @@ public class DataInitUtil {
             stmt.executeUpdate("insert into office(office_name, office_person, phone, address,type,belong_office) values('贵阳源鸿物流有限公司','林伟军','13358215635','贵阳市','分公司',1);");
 
             stmt.executeUpdate("insert into inventory_item(party_id, warehouse_id, product_id,total_quantity) values(1, 1, 4, 100);");
+            stmt.executeUpdate("insert into inventory_item(party_id, warehouse_id, product_id,total_quantity) values(1, 30, 4, 100);");//广电，源鸿分仓
             stmt.executeUpdate("insert into user_login(user_name, password, password_hint, office_id) values('d_user1', '123456', '1-6',1);");
             stmt.executeUpdate("insert into user_login(user_name, password, password_hint) values('d_user2', '123456', '1-6');");
             stmt.executeUpdate("insert into user_login(user_name,c_name, password, password_hint, office_id) values('demo','管理员','123456', '1-6', 3);");
