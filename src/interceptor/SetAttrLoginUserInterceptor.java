66ff445c2ffbf588323a697fa3faa9dac4ff31ac
rayliu <liuyubie@gmail.com>
2015-01-22 16:15:31 +0800

    修改拦截器代码。

--- src/interceptor/SetAttrLoginUserInterceptor.java
+++ src/interceptor/SetAttrLoginUserInterceptor.java
@@ -13,13 +13,14 @@ public class SetAttrLoginUserInterceptor implements Interceptor{
 	@Override
 	public void intercept(ActionInvocation ai) {
 		Subject currentUser = SecurityUtils.getSubject();
-		UserLogin user = UserLogin.dao.findFirst("select * from user_login where user_name=?",currentUser.getPrincipal());
-		if(user.get("c_name") != null && !"".equals(user.get("c_name"))){
-			ai.getController().setAttr("userId", user.get("c_name"));
-		}else{
-			ai.getController().setAttr("userId", currentUser.getPrincipal());
+		if(currentUser.isAuthenticated()){
+			UserLogin user = UserLogin.dao.findFirst("select * from user_login where user_name=?",currentUser.getPrincipal());
+			if(user.get("c_name") != null && !"".equals(user.get("c_name"))){
+				ai.getController().setAttr("userId", user.get("c_name"));
+			}else{
+				ai.getController().setAttr("userId", currentUser.getPrincipal());
+			}
 		}
-		
 		ai.invoke();
 	}
 
