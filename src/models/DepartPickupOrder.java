e836b798656fe1b42414e4e9a8917217671c6f18
back_lanj <back_lanj@eeda123.com>
2015-01-24 18:06:17 +0800

    model对象补充提交

---
+++ src/models/DepartPickupOrder.java
@@ -0,0 +1,7 @@
+package models;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class DepartPickupOrder extends Model<DepartPickupOrder> {
+	public static final DepartPickupOrder dao = new DepartPickupOrder();
+}
