e25777469a2e9dce899710a9335ca11e1c05535d
redluo <red.luo@eeda123.com>
2015-02-06 14:22:02 +0800

    增加一个CustomizeField，用来存放标签的实体类

---
+++ src/models/yh/profile/CustomizeField.java
@@ -0,0 +1,10 @@
+package models.yh.profile;
+
+import models.ArapCostOrder;
+
+import com.jfinal.plugin.activerecord.Model;
+
+@SuppressWarnings("serial")
+public class CustomizeField extends Model<CustomizeField>{
+	 public static final CustomizeField dao = new CustomizeField();
+}
