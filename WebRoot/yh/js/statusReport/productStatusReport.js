90e139b7d2cdf69ffc307ba1265ece50b3f3860a
back_lanj <back_lanj@eeda123.com>
2015-02-10 17:41:41 +0800

    单品查询报表

--- WebRoot/yh/js/statusReport/productStatusReport.js
+++ WebRoot/yh/js/statusReport/productStatusReport.js
@@ -13,24 +13,179 @@ $(document).ready(function() {
     	"oLanguage": {
             "sUrl": "/eeda/dataTables.ch.txt"
         },
-        "sAjaxSource": "/transferOrder/list",
+        "sAjaxSource": "/statusReport/findTransferOrdertatus?order_no=''&customer_id=''"
+						+"&customer_order_no=''&item_no=''&serial_no=''",
         "aoColumns": [   
-            {"mDataProp":"STATUS", "sWidth":"100px"},
-            {"mDataProp":"CNAME", "sWidth":"100px"},
-            {"mDataProp":"ROUTE_FROM", "sWidth":"100px"},
-            {"mDataProp":"ROUTE_TO", "sWidth":"100px"},       	
-            {"mDataProp":"AMOUNT", "sWidth":"100px"},
-            {"mDataProp":"PIECES", "sWidth":"100px"},
-            {"mDataProp":"VOLUME", "sWidth":"100px"},
-            {"mDataProp":"WEIGHT", "sWidth":"100px"},
-            {"mDataProp":"PLANNING_TIME", "sWidth":"100px"},
-            {"mDataProp":"ARRIVAL_TIME", "sWidth":"100px"},
-            {"mDataProp":"ADDRESS", "sWidth":"100px",},
-            {"mDataProp":"SPNAME", "sWidth":"100px"},
-            {"mDataProp":"CUSTOMER_ORDER_NO" ,"sWidth":"100px"},
-            {"mDataProp":"ONAME","sWidth":"100px"}
+            {"mDataProp":"SERIAL_NO", "sWidth":"80px"},
+            {"mDataProp":"ITEM_NO", "sWidth":"100px"},
+            {"mDataProp":"CUSTOMER", "sWidth":"100px"},
+            {"mDataProp":"CUSTOMER_ORDER_NO", "sWidth":"80px"},
+            {"mDataProp":null, "sWidth":"180px",
+            	"fnRender": function(obj) {  
+                		return obj.aData.TRANSFER_NO + "(" + obj.aData.TRANSFER_STATUS + ")";
+                }
+            },       	
+            {"mDataProp":null, "sWidth":"150px",
+            	"fnRender": function(obj) {  
+            		if(obj.aData.PICK_NO != null )
+                		return obj.aData.PICK_NO + "(" + obj.aData.PICK_STATUS + ")";
+            		else
+            			return "";
+                }
+            }, 
+            {"mDataProp":null, "sWidth":"150px",
+            	"fnRender": function(obj) {  
+            		if(obj.aData.DEPART_NO != null )
+            			return obj.aData.DEPART_NO + "(" + obj.aData.DEPART_STATUS + ")";
+            		else
+            			return "";
+                }
+            }, 
+            {"mDataProp":null, "sWidth":"150px",
+            	"fnRender": function(obj) {  
+            		if(obj.aData.CARGO_NATURE == "cargo" && obj.aData.CARGO_NATURE_DETAIL == "cargoNatureDetailNo"){
+            			return obj.aData.DELIVERY_NO;
+            		}else{
+            			if(obj.aData.DELIVERY_NO != null )
+                			return obj.aData.DELIVERY_NO + "(" + obj.aData.DELIVERY_STATUS + ")";
+                		else
+                			return "";
+            		}
+                }
+            }, 
+            {"mDataProp":null, "sWidth":"150px",
+            	"fnRender": function(obj) {  
+            		if(obj.aData.RETURN_NO != null )
+            			return obj.aData.RETURN_NO + "(" + obj.aData.RETURN_STATUS + ")";
+            		else
+            			return "";
+                }
+            }
         ]  
     });	
+    
+    $("#order_no,#item_no,#customer_id,#serial_no,#customer_order_no").on( 'keyup click', function () {
+    	var order_no=$("#order_no").val();
+    	var item_no=$("#item_no").val();
+    	var customer_id=$("#customer_id").val();
+    	var serial_no = $("#serial_no").val();
+    	var customer_order_no = $("#customer_order_no").val();
+    	if(order_no != "" || customer_order_no != ""){
+	    	statusTable.fnSettings().sAjaxSource = "/statusReport/findTransferOrdertatus?order_no="+order_no +"&customer_id="+customer_id
+	    											+"&customer_order_no="+customer_order_no+"&item_no="+item_no+"&serial_no="+serial_no;
+	    	statusTable.fnDraw(); 
+    	}
+    });
+    
+    /*// 获取所有运输单号
+	$('#order_no').on('keyup click', function(){
+		if($("#order_no").val() == "")
+	    	$("#hiddenOfficeId").val("");
+		$.get('/gateIn/searchAllOffice',{"officeName":$(this).val()}, function(data){
+			console.log(data);
+			var orderNoList =$("#orderNoList");
+			orderNoList.empty();
+			for(var i = 0; i < data.length; i++)
+			{
+				orderNoList.append("<li><a tabindex='-1' class='fromLocationItem'  code='"+data[i].ID+"'>"+data[i].ORDER_NO+"</a></li>");
+			}
+		},'json');
+		$("#orderNoList").css({ 
+	    	left:$(this).position().left+"px", 
+	    	top:$(this).position().top+32+"px" 
+	    }); 
+	    $('#orderNoList').show();
+	});
+	
+	// 选中运输单号
+	$('#orderNoList').on('mousedown', '.fromLocationItem', function(e){
+		var id =$(this).attr('code');
+		$('#officeSelect').val($(this).text());
+		$('#orderNoList').hide();
+		$("#hiddenOfficeId").val(id);
+		
+		var customerId = $("#hiddenCustomerId").val();
+		var warehouseId = $("#warehouseId").val();
+	    tab.fnSettings().sAjaxSource ="/stock/stocklist?customerId="+customerId+"&warehouseId="+warehouseId+"&offeceId="+id;
+		tab.fnDraw();
+	});
+	$('#order_no').on('blur', function(){
+		$("#orderNoList").hide();
+	});
+	$('#orderNoList').on('blur', function(){
+		$('#orderNoList').hide();
+	});
+
+	$('#orderNoList').on('mousedown', function(){
+		return false;//阻止事件回流，不触发 $('#spMessage').on('blur'
+	});*/
+    
+    //获取客户的list，选中信息在下方展示其他信息
+	$('#customerMessage').on('keyup click', function(){
+		if($('#customerMessage').val() == "")
+			$("#customer_id").val("");
+		$.get('/customerContract/search', {locationName:$('#customerMessage').val()}, function(data){
+			console.log(data);
+			var customerList =$("#customerList");
+			customerList.empty();
+			for(var i = 0; i < data.length; i++)
+			{
+				var company_name = data[i].COMPANY_NAME;
+				if(company_name == null){
+					company_name = '';
+				}
+				var contact_person = data[i].CONTACT_PERSON;
+				if(contact_person == null){
+					contact_person = '';
+				}
+				var phone = data[i].PHONE;
+				if(phone == null){
+					phone = '';
+				}
+				customerList.append("<li><a tabindex='-1' class='fromLocationItem' chargeType='"+data[i].CHARGE_TYPE+"' payment='"+data[i].PAYMENT+"' partyId='"+data[i].PID+"' location='"+data[i].LOCATION+"' post_code='"+data[i].POSTAL_CODE+"' contact_person='"+data[i].CONTACT_PERSON+"' email='"+data[i].EMAIL+"' phone='"+data[i].PHONE+"' cid='"+data[i].ID+"' address='"+data[i].ADDRESS+"', company_name='"+data[i].COMPANY_NAME+"', >"+company_name+" "+contact_person+" "+phone+"</a></li>");
+			}
+		},'json');
+		$("#customerList").css({ 
+        	left:$(this).position().left+"px", 
+        	top:$(this).position().top+32+"px" 
+        }); 
+        $('#customerList').show();
+        
+        /*var customerId = $("#hiddenCustomerId").val();
+		var warehouseId = $("#warehouseId").val();
+		var offeceId = $("#hiddenOfficeId").val();
+	    tab.fnSettings().sAjaxSource ="/stock/stocklist?customerId="+customerId+"&warehouseId="+warehouseId+"&offeceId="+offeceId;
+		tab.fnDraw();*/
+	});
+
+ 	// 没选中客户，焦点离开，隐藏列表
+	$('#customerMessage').on('blur', function(){
+ 		$('#customerList').hide();
+ 	});
+
+	//当用户只点击了滚动条，没选客户，再点击页面别的地方时，隐藏列表
+	$('#customerList').on('blur', function(){
+ 		$('#customerList').hide();
+ 	});
+
+	$('#customerList').on('mousedown', function(){
+		return false;//阻止事件回流，不触发 $('#spMessage').on('blur'
+	});
+	// 选中客户
+	$('#customerList').on('mousedown', '.fromLocationItem', function(e){
+		var message = $(this).text();
+		var customerId = $(this).attr('partyId');
+		var warehouseId = $("#warehouseId").val();
+		var offeceId = $("#hiddenOfficeId").val();
+		$('#customerMessage').val(message.substring(0, message.indexOf(" ")));
+		$("#hiddenCustomerId").val($(this).attr('partyId'));
+		$('#customerList').hide();
+		
+		/*tab.fnSettings().sAjaxSource ="/stock/stocklist?customerId="+customerId+"&warehouseId="+warehouseId+"&offeceId="+offeceId;
+		tab.fnDraw();*/
+    }); 
+    
+    
     
 });
     
--- src/controllers/yh/statusReport/StatusReportColler.java
+++ src/controllers/yh/statusReport/StatusReportColler.java
@@ -1,8 +1,11 @@
 package controllers.yh.statusReport;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import models.TransferOrder;
 
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
@@ -23,50 +26,175 @@ public class StatusReportColler extends Controller{
 		render("/yh/statusReport/productStatusReport.html");
 	}
 	
-	public void accountReceivable() {
-		String id = getPara();
-
+	public void findTransferOrdertatus() {
+		String order_no = getPara("order_no");
+		String customer_id = getPara("customer_id");
+		String customer_order_no = getPara("customer_order_no");
+		String item_no = getPara("item_no");
+		String serial_no = getPara("serial_no");
 		String sLimit = "";
-		if (id == null || id.equals("")) {
-			Map orderMap = new HashMap();
-			orderMap.put("sEcho", 0);
-			orderMap.put("iTotalRecords", 0);
-			orderMap.put("iTotalDisplayRecords", 0);
-			orderMap.put("aaData", null);
-			renderJson(orderMap);
-			return;
-		}
+		
 		String pageIndex = getPara("sEcho");
 		if (getPara("iDisplayStart") != null
 				&& getPara("iDisplayLength") != null) {
 			sLimit = " LIMIT " + getPara("iDisplayStart") + ", "
 					+ getPara("iDisplayLength");
 		}
-
 		// 获取总条数
-		String totalWhere = "";
-		String sql = "select count(0) total from transfer_order_fin_item d left join fin_item f on d.fin_item_id = f.id left join transfer_order t on t.id = d.order_id where d.order_id ='"
-				+ id + "' and f.type='应收'"; //and f.type='应收' TODO： 有问题
-		Record rec = Db.findFirst(sql + totalWhere);
-		logger.debug("total records:" + rec.getLong("total"));
-
-		// 获取当前页的数据
-		/*List<Record> orders = Db
-				.find("select d.*,f.name,t.order_no as transferOrderNo from transfer_order_fin_item d left join fin_item f on d.fin_item_id = f.id left join transfer_order t on t.id = d.order_id where d.order_id ='"
-						+ id + "' and f.type='应收'");*/
-		List<Record> orders =Db.find("select d.*, f.name, t.order_no as transferOrderNo from "
-				+ "transfer_order_fin_item d "
-				+ "left join fin_item f on d.fin_item_id = f.id "
-				//+ "LEFT JOIN return_order r ON d.order_id = r.id "
-				+ "left join transfer_order t on t.id = d.order_id "
-				+ "where d.order_id = " + id + " and f.type = '应收'");
+		String totalSql = "";
+		String sql = "";
+		TransferOrder order = null;
 		Map orderMap = new HashMap();
-		orderMap.put("sEcho", pageIndex);
-		orderMap.put("iTotalRecords", rec.getLong("total"));
-		orderMap.put("iTotalDisplayRecords", rec.getLong("total"));
-
-		orderMap.put("aaData", orders);
-
+		/*if((order_no == null || order_no.equals("")) && (customer_order_no == null || customer_order_no.equals(""))) {
+			orderMap.put("sEcho", 0);
+			orderMap.put("iTotalRecords", 0);
+			orderMap.put("iTotalDisplayRecords", 0);
+			orderMap.put("aaData", null);
+			renderJson(orderMap);
+			return;
+		}else{*/
+			if(order_no != null || !"".equals(order_no)){
+				order = TransferOrder.dao.findFirst("select * from transfer_order where order_no = '" + order_no + "';");
+			}else if(customer_order_no != null || !"".equals(customer_order_no)){
+				order = TransferOrder.dao.findFirst("select * from transfer_order where customer_order_no = '" + customer_order_no + "';");
+			}
+		//}
+		if(order != null){
+			long order_id = order.getLong("id");
+			//ATM，普货有单品
+			if("ATM".equals(order.get("cargo_nature")) || (!"ATM".equals(order.get("cargo_nature")) && "cargoNatureDetailYes".equals(order.get("cargo_nature_detail")))){
+				totalSql = "select count(0) total from transfer_order_item_detail toid"
+						+ " left join transfer_order tor on tor.id = toid.order_id"
+						+ " left join party p on p.id = tor.customer_id"
+						+ " left join contact c on c.id = p.contact_id"
+						+ " left join depart_transfer pick on pick.id = toid.pickup_id"
+						+ " left join depart_order pkdo on pkdo.id = toid.pickup_id"
+						+ " left join depart_transfer depart on depart.id = toid.depart_id"
+						+ " left join depart_order dedo on dedo.id = toid.depart_id"
+						+ " left join delivery_order dor on dor.id = toid.delivery_id"
+						+ " left join return_order ro on ro.delivery_order_id = dor.id"
+						+ " where toid.order_id = '" + order_id + "'";
+				
+				sql ="select toid.serial_no,item_no,tor.order_no transfer_no,tor.status transfer_status,tor.customer_order_no,tor.cargo_nature,tor.cargo_nature_detail,c.abbr customer,pkdo.depart_no pick_no,pkdo.status pick_status,"
+						+ " dedo.depart_no depart_no,dedo.status depart_status,dor.order_no delivery_no,dor.status delivery_status,ro.order_no return_no,ro.transaction_status return_status from transfer_order_item_detail toid"
+						+ " left join transfer_order tor on tor.id = toid.order_id"
+						+ " left join party p on p.id = tor.customer_id"
+						+ " left join contact c on c.id = p.contact_id"
+						+ " left join depart_transfer pick on pick.id = toid.pickup_id"
+						+ " left join depart_order pkdo on pkdo.id = toid.pickup_id"
+						+ " left join depart_transfer depart on depart.id = toid.depart_id"
+						+ " left join depart_order dedo on dedo.id = toid.depart_id"
+						+ " left join delivery_order dor on dor.id = toid.delivery_id"
+						+ " left join return_order ro on ro.delivery_order_id = dor.id"
+						+ " where toid.order_id = '" + order_id + "'";
+							
+				//有序列号时
+				if(!"".equals(serial_no) && serial_no != null){
+					totalSql = totalSql + " and toid.serial_no = '" + serial_no + "'";
+					sql = sql + " and toid.serial_no = '" + serial_no + "'";
+				}
+				//有货品型号时
+				if(!"".equals(item_no) && item_no != null){
+					totalSql = totalSql + "and toid.item_no = '" + item_no + "'";
+					sql = sql + "and toid.item_no = '" + item_no + "'";
+				}
+				//有客户时
+				if(!"".equals(customer_id) && customer_id != null){
+					totalSql = totalSql + "and tor.customer_id = '" + customer_id + "'";
+					sql = sql + "and tor.customer_id = '" + customer_id + "'";
+				}
+			}else{
+				/*//普货没单品
+				totalSql = "select count(0) total from transfer_order_item toid "
+						+ " left join transfer_order tor on tor.id = toid.order_id"
+						+ " left join party p on p.id = tor.customer_id"
+						+ " left join contact c on c.id = p.contact_id"
+						+ " left join depart_transfer pick on pick.order_id = tor.id"
+						+ " left join depart_order pkdo on pkdo.id = pick.pickup_id"
+						+ " left join depart_transfer depart on depart.order_id = tor.id"
+						+ " left join depart_order dedo on dedo.id = depart.depart_id"
+						+ " left join delivery_order_item dori on dori.transfer_order_id = tor.id"
+						+ " left join delivery_order dor on dor.id = dori.delivery_id"
+						+ " left join return_order ro on ro.transfer_order_id = tor.id"
+						+ " where tor.id = '" + order_id + "'";
+				
+				sql = "select (select '' ) serial_no,item_no,tor.order_no transfer_no,tor.status transfer_status,tor.customer_order_no,c.abbr customer,pkdo.depart_no pick_no,pkdo.status pick_status,dedo.depart_no depart_no,dedo.status depart_status,"
+						+ " dor.order_no delivery_no,dor.status delivery_status,ro.order_no return_no,ro.transaction_status return_status from transfer_order_item toid "
+						+ " left join transfer_order tor on tor.id = toid.order_id"
+						+ " left join party p on p.id = tor.customer_id"
+						+ " left join contact c on c.id = p.contact_id"
+						+ " left join depart_transfer pick on pick.order_id = tor.id"
+						+ " left join depart_order pkdo on pkdo.id = pick.pickup_id"
+						+ " left join depart_transfer depart on depart.order_id = tor.id"
+						+ " left join depart_order dedo on dedo.id = depart.depart_id"
+						+ " left join delivery_order_item dori on dori.transfer_order_id = tor.id"
+						+ " left join delivery_order dor on dor.id = dori.delivery_id"
+						+ " left join return_order ro on ro.transfer_order_id = tor.id"
+						+ " where tor.id = '" + order_id + "'";
+				*/
+				totalSql = "select count(0) total from transfer_order_item toid "
+						+ " left join transfer_order tor on tor.id = toid.order_id"
+						+ " left join party p on p.id = tor.customer_id"
+						+ " left join contact c on c.id = p.contact_id"
+						+ " where tor.id = '" + order_id + "'";
+				
+				sql = "select (select '' ) serial_no,item_no,tor.order_no transfer_no,tor.status transfer_status,tor.customer_order_no,tor.cargo_nature,tor.cargo_nature_detail,c.abbr customer,"
+						+ " (select dd.depart_no from depart_order dd left join depart_pickup dp on dp.pickup_id = dd.id where dp.order_id = tor.id ) pick_no,"
+						+ " (select dd.status from depart_order dd left join depart_pickup dp on dp.pickup_id = dd.id where dp.order_id = tor.id ) pick_status,"
+						+ " (select dd.depart_no from depart_order dd left join depart_pickup dp on dp.depart_id = dd.id where dp.order_id = tor.id ) depart_no,"
+						+ " (select dd.status from depart_order dd left join depart_pickup dp on dp.depart_id = dd.id where dp.order_id = tor.id ) depart_status,"
+						+ " (select group_concat(concat(concat(deor.order_no,'('),deor.status,')') separator '<br>') from delivery_order deor left join delivery_order_item doi on doi.delivery_id = deor.id where doi.transfer_order_id = tor.id) delivery_no,"
+						+ " (select ror.order_no from return_order ror where ror.transfer_order_id = tor.id) return_no,"
+						+ " (select ror.transaction_status from return_order ror where ror.transfer_order_id = tor.id) return_status from transfer_order_item toid "
+						+ " left join transfer_order tor on tor.id = toid.order_id"
+						+ " left join party p on p.id = tor.customer_id"
+						+ " left join contact c on c.id = p.contact_id"
+						+ " where tor.id = '" + order_id + "'";
+				
+				//有货品型号时
+				if(!"".equals(item_no) && item_no != null){
+					totalSql = totalSql + "and toid.item_no = '" + item_no + "'";
+					sql = sql + "and toid.item_no = '" + item_no + "'";
+				}
+				//有客户时
+				if(!"".equals(customer_id) && customer_id != null){
+					totalSql = totalSql + "and tor.customer_id = '" + customer_id + "'";
+					sql = sql + "and tor.customer_id = '" + customer_id + "'";
+				}
+			}
+			
+			// 获取总条数
+			Record rec = Db.findFirst(totalSql);
+			logger.debug("total records:" + rec.getLong("total"));
+			// 获取当前页的数据
+			List<Record> orders =Db.find(sql);
+			orderMap.put("sEcho", pageIndex);
+			orderMap.put("iTotalRecords", rec.getLong("total"));
+			orderMap.put("iTotalDisplayRecords", rec.getLong("total"));
+			orderMap.put("aaData", orders);
+		}else{
+			orderMap.put("sEcho", 0);
+			orderMap.put("iTotalRecords", 0);
+			orderMap.put("iTotalDisplayRecords", 0);
+			orderMap.put("aaData", null);
+		}
 		renderJson(orderMap);
 	}
+	
+	public void findAllTransferOrderNo(){
+		String locationName = getPara("locationName");
+	        
+        List<Record> locationList = Collections.EMPTY_LIST;
+        if (locationName.trim().length() > 0) {
+            locationList = Db
+                    .find("select *,p.id as pid from party p,contact c where p.contact_id = c.id and p.party_type = 'CUSTOMER' and c.company_name like '%"+locationName+"%' and p.id in (select customer_id from user_customer where user_name='"+currentUser.getPrincipal()+"')");
+
+        } else {
+            locationList = Db.find("select *,p.id as pid from party p,contact c where p.contact_id = c.id and p.party_type = 'CUSTOMER'  and p.id in (select customer_id from user_customer where user_name='"+currentUser.getPrincipal()+"')");
+        }
+        renderJson(locationList);
+	}
+	
+	
+	
 }
