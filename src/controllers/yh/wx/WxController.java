fe1c44d377a6eb2deed9f8c46055e2d48eb9b6bf
Ray Liu <liuyubie@gmail.com>
2015-02-21 15:10:56 +0800

    微信demo页面, 用错了controller

--- src/controllers/yh/wx/WxController.java
+++ src/controllers/yh/wx/WxController.java
@@ -4,12 +4,38 @@ import java.util.Map;
 
 import com.jfinal.core.Controller;
 import com.jfinal.kit.HttpKit;
+import com.jfinal.kit.PropKit;
 import com.jfinal.weixin.demo.SignKit;
 import com.jfinal.weixin.sdk.api.AccessTokenApi;
+import com.jfinal.weixin.sdk.api.ApiConfig;
 import com.jfinal.weixin.sdk.api.ApiResult;
 import com.jfinal.weixin.sdk.api.JsTicketApi;
+import com.jfinal.weixin.sdk.jfinal.ApiController;
 
-public class WxController extends Controller {
+public class WxController extends ApiController {
+	
+	/**
+	 * 如果要支持多公众账号，只需要在此返回各个公众号对应的  ApiConfig 对象即可
+	 * 可以通过在请求 url 中挂参数来动态从数据库中获取 ApiConfig 属性值
+	 */
+	public ApiConfig getApiConfig() {
+		ApiConfig ac = new ApiConfig();
+		
+		// 配置微信 API 相关常量
+		ac.setToken(PropKit.get("token"));
+		ac.setAppId(PropKit.get("appId"));
+		ac.setAppSecret(PropKit.get("appSecret"));
+		
+		/**
+		 *  是否对消息进行加密，对应于微信平台的消息加解密方式：
+		 *  1：true进行加密且必须配置 encodingAesKey
+		 *  2：false采用明文模式，同时也支持混合模式
+		 */
+		ac.setEncryptMessage(PropKit.getBoolean("encryptMessage", false));
+		ac.setEncodingAesKey(PropKit.get("encodingAesKey", "setting it in config file"));
+		return ac;
+	}
+	
 	//微信JS demo页面，方便参考
 	public void demo() {
 		String jsapi_ticket = JsTicketApi.getJsTicket().getJsTicket();
