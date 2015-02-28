050d9c0a1aae8e1f11f3b970324d27de94d4642c
back_lanj <back_lanj@eeda123.com>
2015-02-28 17:49:12 +0800

    手机上传图片调整

--- WebRoot/yh/js/wx/ro_filing.js
+++ WebRoot/yh/js/wx/ro_filing.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     $.post('/wx/getRo/'+orderNo,function(data){
            
            if(data.ORDER_NO){
-              $('#orderDesc').text('回单号码确认存在，请从相册中选择照片上传。');
+              $('#orderDesc').text('回单号码确认存在，请从相册中选择照片上传');
               $('#returnId').val(data.ID);
               initFileupload();
            }else{
-              $('#orderDesc').text('回单号码不存在，请重新查询。');
+              $('#orderDesc').text('回单号码不存在，请重新查询');
            }
            $('#orderDesc').show();
         },'json');
@@ -20,7 +20,8 @@ $(document).ready(function() {
   //保存图片
     $("#uploadBtn").click(function(e){
       if($("#returnId").val().length==0){
-        alert('回单号码不存在，请重新查询.');
+        //alert('回单号码不存在，请重新查询.');
+        $("#uploadDesc").text("请先查询有效的回单号码").show();
         return;
       }
       $("#fileupload").click();
@@ -33,21 +34,23 @@ $(document).ready(function() {
         url: '/wx/saveFile?return_id='+$("#returnId").val(),//上传地址
         done: function (e, data) {
           if(data.result.result = "true"){
-            alert("上传成功！");
+        	$("#uploadBtn").text("上传图片");
+        	$('#uploadDesc').text('上传成功！').show();
+            //alert("上传成功！");
             console.log("data.result.cause:"+data.result.cause);
             //console.log("data.result.cause:"+data.result.cause+",parseJSON:"+$.parseJSON(data.result.cause));
             var files = $.parseJSON(data.result.cause);
             var showPictures = $("#showPictures");
             showPictures.empty();
             $.each(data.result.cause,function(name,value) {
-              showPictures.append('<div style="width:220px;height:220px;float:left;" ><img src="/upload/fileupload/'+value.FILE_PATH+'" alt="" style="width:200px;height:200px;"><p><a class="picturedel" picture_id="'+value.ID+'" >删除</a></p></div>');
-                });
+            	showPictures.append('<div style="width:200px;height:210px;float:left;" ><img src="/upload/fileupload/'+value.FILE_PATH+'" alt="" class="imgSign" style="width:180px;height:180px;"><p><a class="picturedel" picture_id="'+value.ID+'" >删除</a></p></div>');
+            });
           }else{
             $("#centerBody").empty().append("<h4>"+data.result.cause+"</h4>");
           }
-          $("#footer").show();
         },  
         progressall: function (e, data) {//设置上传进度事件的回调函数  
+        	$("#uploadBtn").text("上传中.....");
           //$.scojs_message('上传中', $.scojs_message.TYPE_OK);
           //$('#myModal').modal('show');
           //$("#footer").hide();
