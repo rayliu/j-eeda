fbb3a2373bfec82b90a18e2ba21712cca08b5b9a
redluo <red.luo@eeda123.com>
2015-03-17 18:02:42 +0800

    客户列表宽度控制

--- WebRoot/yh/js/profile/customer/list.js
+++ WebRoot/yh/js/profile/customer/list.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
    var dataTable= $('#eeda-table').dataTable({
 	   "bFilter": false, //不需要默认的搜索框 
 	   //"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r><'datatable-scroll't><'row-fluid'<'span12'i><'span12 center'p>>",
         //"sPaginationType": "bootstrap",
         "iDisplayLength": 10,
     	"oLanguage": {
