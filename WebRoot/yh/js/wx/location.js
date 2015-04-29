ee7d76e4edfb1fb380c531553e7b00c614289ec5
Ray Liu <liuyubie@gmail.com>
2015-04-29 22:56:33 +0800

    微信获取地点信息

--- WebRoot/yh/js/wx/location.js
+++ WebRoot/yh/js/wx/location.js
@@ -1,30 +1,46 @@
 
 wx.ready(function () {
-  // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
-  document.querySelector('#checkJsApi').onclick = function () {
-    wx.checkJsApi({
-      jsApiList: [
-        'getNetworkType',
-        'previewImage'
-      ],
-      success: function (res) {
-        alert(JSON.stringify(res));
-      }
-    });
-  };
+  var geocoder,map, marker = null;
+  geocoder = new soso.maps.Geocoder();
 
+  var getLocationInfo= function(latlng, callback){
+      var res = null;
+      var latLng = new soso.maps.LatLng(latlng.latitude, latlng.longitude);
+      geocoder.geocode({'location': latLng}, function(results, status) {
+            if (status == soso.maps.GeocoderStatus.OK) {
+                callback(latlng, results.address);
+            } else {
+                alert("检索没有结果，原因: " + status);
+            }
+      });
+      return res;
+  };
   
   // 7 地理位置接口
   // 7.1 查看地理位置
   document.querySelector('#openLocation').onclick = function () {
-    wx.openLocation({
-      latitude: 23.099994,
-      longitude: 113.324520,
-      name: 'TIT 创意园',
-      address: '广州市海珠区新港中路 397 号',
-      scale: 14,
-      infoUrl: 'http://weixin.qq.com'
+     wx.getLocation({
+      success: function (res) {
+        //alert(JSON.stringify(res));
+        
+        var openLoc=function(res, address){
+          wx.openLocation({
+            latitude: res.latitude,
+            longitude: res.longitude,
+            // name: 'TIT 创意园',
+            address: address,
+            scale: 14,
+            infoUrl: 'http://weixin.qq.com'
+          });
+        };
+
+        getLocationInfo(res, openLoc)
+      },
+      cancel: function (res) {
+        alert('用户拒绝授权获取地理位置');
+      }
     });
+    
   };
 
   // 7.2 获取当前地理位置
@@ -32,6 +48,7 @@ wx.ready(function () {
     wx.getLocation({
       success: function (res) {
         alert(JSON.stringify(res));
+        getLocationInfo(res);
       },
       cancel: function (res) {
         alert('用户拒绝授权获取地理位置');
