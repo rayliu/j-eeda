b053e3bbcf7c1de64f16e011fe53d2a875e67d71
redluo <red.luo@eeda123.com>
2015-05-12 11:30:19 +0800

    pdf打印优化一

--- src/controllers/yh/report/ReportController.java
+++ src/controllers/yh/report/ReportController.java
@@ -6,8 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import models.TransferOrder;
 import models.TransferOrderItemDetail;
@@ -17,10 +15,11 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 
 import org.apache.log4j.Logger;
-import org.jfree.util.Log;
 
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.DbKit;
+
+import controllers.yh.util.PrintPatterns;
 
 public class ReportController extends Controller {
 
@@ -29,70 +28,45 @@ public class ReportController extends Controller {
     public void index() {
         
     }
-    private String print(String order_no,String name,Object id,Object serial_no){
-    	
-    	 String fileName = "report/";
-         String outFileName="WebRoot/download/";
-         File file = new File(outFileName);
-         if(!file.exists()){
-        	 file.mkdir();
-         }
-         fileName +=name;
-         Date date = new Date();
-         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
-         
-    	 if(name.equals("checkOrder.jasper")){
-    		 outFileName +="供应商对账单_"+format.format(date)+".pdf";
-    	 }else if(name.equals("payment.jasper")){
-         	outFileName +="付款申请单_"+format.format(date)+".pdf";
-    	 }else if(name.equals("guoguang_one.jasper") || name.equals("guoguang_n.jasper")){
-    		 outFileName +="国光标准单-" + serial_no + "-" +format.format(date)+".pdf";
-    	 }else if(name.equals("nonghang_one.jasper") || name.equals("nonghang_n.jasper")){
-    		 outFileName +="农行-" + serial_no + "-" +format.format(date)+".pdf";
-    	 }else if(name.equals("china_post_one.jaspe") || name.equals("china_post_n.jaspe")){
-    		 outFileName +="中国邮政-" + serial_no + "-" +format.format(date)+".pdf";
-    	 }else{
-    		 outFileName +="中国邮储-" + serial_no + "-" +format.format(date)+".pdf";
-    	 }
-    	HashMap<String, Object> hm = new HashMap<String, Object>();
-    	hm.put("order_no", order_no);
-    	if( !"0".equals(id)){
-        	hm.put("id", id);
-    	}
-		try {
-			JasperPrint print = JasperFillManager.fillReport(fileName, hm, DbKit.getConfig().getConnection());
-			JasperExportManager.exportReportToPdfFile(print, outFileName);
-		} catch (JRException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return outFileName;
-    }
+
     public void printCheckOrder(){
     	String order_no = getPara("order_no");
-    	String file = print(order_no,"checkOrder.jasper","0","");
+    	String fileName ="report/checkOrder.jasper";
+    	String outFileName = "WebRoot/download/供应商对账单";
+    	HashMap<String, Object> hm = new HashMap<String, Object>();
+    	hm.put("order_no", order_no);
+    	String file = PrintPatterns.getInstance().print(fileName,outFileName,hm);
     	renderText(file.substring(7));
     }
    public void printPayMent(){
 	   String order_no = getPara("order_no");
-	   String file = print(order_no,"payment.jasper","0","");
+	   String fileName ="report/checkOrder.jasper";
+	   String outFileName = "WebRoot/download/付款申请单";
+	   HashMap<String, Object> hm = new HashMap<String, Object>();
+	   hm.put("order_no", order_no);
+	   String file = PrintPatterns.getInstance().print(fileName,outFileName,hm);
 	   renderText(file.substring(7));
    }
    public void printSign(){
 	   String type = getPara("sign");
 	   String order_no = getPara("order_no");
 	   String muban = type + ".jasper";
-	   /*if(type.equals("signGuoguang")){
-		   muban = "guoguang.jasper";
-	   }else if(type.equals("signNonghang")){
-		   muban = "nonghang.jasper";
-	   }else if(type.equals("post")){
-		   muban = "china_post.jasper";
+	  
+	   String fileName ="report/" + muban;
+	   String outFileName = "WebRoot/download/";
+	   
+	   if(type.contains("guoguang")){
+		   outFileName  +="国光标准单";
+	   }else if(type.contains("nonghang")){
+		   outFileName +="农行";
+	   }else if(type.contains("china_post")){
+		   outFileName +="中国邮政";
 	   }else{
-		   muban = "china_postal.jasper";
-	   }*/
+		   outFileName +="中国邮储";
+	   }
+	   
+	   HashMap<String, Object> hm = new HashMap<String, Object>();
+	   hm.put("order_no", order_no);
 	   boolean is_one = muban.contains("_one");
 	   TransferOrder to = TransferOrder.dao.findFirst("select id from transfer_order where order_no = ?",order_no);
 	   List<TransferOrderItemDetail> list = TransferOrderItemDetail.dao.find("select id,serial_no from transfer_order_item_detail where order_id =?",to.get("id"));
@@ -102,12 +76,23 @@ public class ReportController extends Controller {
 		   
 			   for(int i=0;i<list.size();i++){
 				   if(is_one){
-					   String file = print(order_no,muban,list.get(i).get("id"),list.get(i).get("serial_no"));
+					   hm.put("id", list.get(i).get("id"));
+					   if(list.get(i).get("serial_no") != null){
+						   outFileName += "-" + list.get(i).get("serial_no");
+					   }
+					   
+					   String file = PrintPatterns.getInstance().print(fileName,outFileName,hm);
+					  
 					   buffer.append(file.substring(7));
 					   buffer.append(",");
 					   break;
 				   }else{
-					   String file = print(order_no,muban,list.get(i).get("id"),list.get(i).get("serial_no"));
+					   hm.put("id", list.get(i).get("id"));
+					   if(list.get(i).get("serial_no") != null){
+						   outFileName += "-" + list.get(i).get("serial_no");
+					   }
+					   
+					   String file = PrintPatterns.getInstance().print(fileName,outFileName,hm);
 						try {
 							   Thread.sleep(200);
 							} catch (InterruptedException e) {
@@ -123,7 +108,7 @@ public class ReportController extends Controller {
 		   
 		   renderText(buffer.toString());
 	   }else{
-		   String file = print(order_no,muban,"0","1");
+		   String file = PrintPatterns.getInstance().print(fileName,outFileName,hm);
 		   renderText(file.substring(7)); 
 	   }
 	    
---
+++ src/controllers/yh/util/PrintPatterns.java
@@ -0,0 +1,48 @@
+package controllers.yh.util;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+import com.jfinal.plugin.activerecord.DbKit;
+
+public class PrintPatterns {
+	/**
+	 * fileName:当前打印的模板
+	 * outFileName:当前打印的名称
+	 */
+	private PrintPatterns(){
+		
+	}
+	public static PrintPatterns getInstance(){
+		return new PrintPatterns();
+	}
+	
+	public String print(String fileName,String outFileName,HashMap<String, Object> hm){		
+        File file = new File("WebRoot/download");
+        if(!file.exists()){
+       	 file.mkdir();
+        }
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+        
+        outFileName += "-" + format.format(date) + ".pdf";
+		try {
+			JasperPrint print = JasperFillManager.fillReport(fileName, hm, DbKit.getConfig().getConnection());
+			JasperExportManager.exportReportToPdfFile(print, outFileName);
+		} catch (JRException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return outFileName;
+   }
+}
