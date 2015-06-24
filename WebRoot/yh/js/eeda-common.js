61b56696c9bfd986cccb31485aa7575c907bae67
redluo <red.luo@eeda123.com>
2015-06-24 14:27:06 +0800

    会话失效，未做完

--- WebRoot/yh/js/eeda-common.js
+++ WebRoot/yh/js/eeda-common.js
@@ -9,3 +9,11 @@
  var contactUrl=function(str,id){
 	 refreshUrl(window.location.protocol + "//" + window.location.host+window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')+1)+str+"="+id);
  };
+ 
+
+ window.onupload=function(){
+	 var stateObj = {needUpdateSTO:"11"};
+	 if(Ext.isFunction(window.history.pushState)){
+		 window.history.pushState(stateObj,"","");
+	 }
+ };
