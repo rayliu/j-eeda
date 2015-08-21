28ea36f1d6d0bd1f683b4035965f9aa203add417
sean <sean.wang@eeda123.com>
2015-08-21 14:28:16 +0800

    对账人改成中文

--- src/controllers/yh/arap/ap/CostPreInvoiceOrderController.java
+++ src/controllers/yh/arap/ap/CostPreInvoiceOrderController.java
@@ -717,7 +717,7 @@ public class CostPreInvoiceOrderController extends Controller {
 		logger.debug("total records:" + rec.getLong("total"));
 
 		String sql = "select aco.*,c.abbr cname, (select group_concat(acai.invoice_no) from arap_cost_order aaia left join arap_cost_order_invoice_no acai on acai.cost_order_id = aaia.id where aaia.id = aco.id) invoice_no,"
-				+ " (select group_concat(cost_invoice_no.invoice_no separator ',') from arap_cost_invoice_item_invoice_no cost_invoice_no where cost_invoice_no.invoice_id = appl_order.id) all_invoice_no,ul.user_name creator_name,"
+				+ " (select group_concat(cost_invoice_no.invoice_no separator ',') from arap_cost_invoice_item_invoice_no cost_invoice_no where cost_invoice_no.invoice_id = appl_order.id) all_invoice_no,ifnull(ul.c_name,ul.user_name) creator_name,"
 				+ " ( SELECT ifnull(sum(caor.pay_amount), 0) total_pay FROM cost_application_order_rel caor"
 				+ " WHERE caor.cost_order_id = aco.id ) total_pay ,"
 				+ " ( SELECT caor.pay_amount this_pay FROM cost_application_order_rel caor"
