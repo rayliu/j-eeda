cb75955f4dbbfc37e3b7fcab02aa93fe75b6044b
doukevin <kevin.dou@eeda123.com>
2015-09-17 10:54:16 +0800

    chargeApplication  model

---
+++ src/models/ChargeApplicationOrderRel.java
@@ -0,0 +1,7 @@
+package models;
+
+import com.jfinal.plugin.activerecord.Model;
+@SuppressWarnings("serial")
+public class ChargeApplicationOrderRel extends Model<ChargeApplicationOrderRel> {
+	public static final ChargeApplicationOrderRel dao = new ChargeApplicationOrderRel();
+}
