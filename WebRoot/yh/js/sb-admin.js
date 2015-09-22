8390c84638c825df218917ec6da1f976bf083c73
Ray Liu <liuyubie@gmail.com>
2015-09-22 17:43:53 +0800

    左侧菜单隐藏功能

--- WebRoot/yh/js/sb-admin.js
+++ WebRoot/yh/js/sb-admin.js
@@ -1,7 +1,5 @@
 $(function() {
-
     $('#side-menu').metisMenu();
-
 });
 
 //Loads the correct sidebar on window load,
@@ -14,4 +12,52 @@ $(function() {
             $('div.sidebar-collapse').removeClass('collapse')
         }
     })
-})
+});
+
+$(document).ready(function() {
+	
+
+    var showSideBar=function(){
+    	$('#left_side_bar').show();
+		$('#page-wrapper').css('margin-left', '250px');
+		$('#hide_menu_btn').css('left', '250px');
+		$('#hide_menu_icon').removeClass('fa-angle-double-right');
+	    $('#hide_menu_icon').addClass('fa-angle-double-left');
+	    if(!!window.localStorage){
+            localStorage.setItem("is_show_eeda_side_bar", true);
+        }
+    };
+
+    var hideSideBar=function(){
+    	$('#left_side_bar').hide();
+		$('#page-wrapper').css('margin', '0');
+		$('#hide_menu_btn').css('left', '0px');
+		$('#hide_menu_icon').removeClass('fa-angle-double-left');
+	    $('#hide_menu_icon').addClass('fa-angle-double-right');
+
+	    if(!!window.localStorage){
+            localStorage.setItem("is_show_eeda_side_bar", false);
+        }
+    };
+
+	$('#hide_menu_btn').click(function(){
+		if ($('#left_side_bar:visible').length > 0){//the element is visible
+			hideSideBar();
+		}else{
+			showSideBar();
+		}
+	});
+
+	var loadSideBar=function(){
+        if(!!window.localStorage){//查询条件处理
+            var is_show_eeda_side_bar = localStorage.getItem('is_show_eeda_side_bar');
+            if(is_show_eeda_side_bar=='false'){
+				hideSideBar();
+            }else{
+            	showSideBar();
+            }
+        }
+    };
+
+    loadSideBar();
+});
