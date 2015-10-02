2ae2124d6d8e5c81b3956d61721eaed06aea53ad
Ray Liu <liuyubie@gmail.com>
2015-10-02 22:37:57 +0800

    APP controller 运输单查询

--- src/controllers/yh/app/AppController.java
+++ src/controllers/yh/app/AppController.java
@@ -1,5 +1,6 @@
 package controllers.yh.app;
 
+import java.util.HashMap;
 import java.util.List;
 
 import models.TransferOrder;
@@ -7,17 +8,34 @@ import models.TransferOrder;
 import com.jfinal.core.Controller;
 import com.jfinal.log.Logger;
 //import org.json.JSONObject;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 
 public class AppController extends Controller {
 	private Logger logger = Logger.getLogger(AppController.class);
+	private static int pageInterval = 20;//每页20行
 	
 	public void searchOrder(){
 		String para = getPara();
 		if(para==null)
 			para="";
-		logger.debug("para:"+para);
-		String sql = "select * from transfer_order where order_no like '%"+para.trim()+"%'";
-		List<TransferOrder> list = TransferOrder.dao.find(sql);
-		renderJson(list);
+		
+		Integer pageIndex = getParaToInt("pageIndex");
+		if(pageIndex==null)
+		    pageIndex=0;
+		String sql = "select tor.*, c.abbr customer_name from transfer_order tor, contact c where tor.customer_id = c.id and tor.order_no like '%"+para.trim()+"%'";
+		
+		Record totalRec = Db.findFirst("select count(1) total from ("+sql+") A");
+		
+		List<TransferOrder> list = TransferOrder.dao.find(sql+" limit "+ (pageIndex*pageInterval)+", "+pageInterval);
+		
+
+		HashMap transferOrderListMap = new HashMap();
+        transferOrderListMap.put("pageIndex", pageIndex);
+        transferOrderListMap.put("iTotalRecords", totalRec.getLong("total").intValue());
+
+        transferOrderListMap.put("orderList", list);
+        
+		renderJson(transferOrderListMap);
 	}
 }
