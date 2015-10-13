7ffe1d98210234062560e10541e11b5f757ad248
Ray Liu <liuyubie@gmail.com>
2015-10-13 16:46:47 +0800

    监控每个URL的请求IP

--- src/handler/UrlHanlder.java
+++ src/handler/UrlHanlder.java
@@ -10,7 +10,16 @@ public class UrlHanlder extends Handler {
 	private Logger logger = Logger.getLogger(UrlHanlder.class);
 	@Override
 	public void handle(String target, HttpServletRequest request, HttpServletResponse response, boolean[] isHandled) {
-		//logger.debug("dynamic handle url:"+target);
+		String ip = request.getRemoteAddr();//返回发出请求的IP地址
+	    String params = request.getQueryString();//返回请求行中的参数部分
+	    String host=request.getRemoteHost();//返回发出请求的客户机的主机名
+	    int port =request.getRemotePort();//返回发出请求的客户机的端口号。
+		
+	    logger.debug("handle url: "+target);
+	    logger.debug("IP: "+ip);
+	    logger.debug("params: "+params);
+	    logger.debug("host: "+host+":"+port);
+	    logger.debug("----------------------------------------------------------");
 		//login page handle
 		if(target.indexOf("/login")>0 && target.split("/").length>2){
 			target = target.substring(target.indexOf("/", 2));
