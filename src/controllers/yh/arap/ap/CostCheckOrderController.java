abc29b4c4287d10e7488436b2b14cd48fc030648
doukevin <kevin.dou@eeda123.com>
2015-10-28 22:38:25 +0800

    应付对账单回显BUG

--- src/controllers/yh/arap/ap/CostCheckOrderController.java
+++ src/controllers/yh/arap/ap/CostCheckOrderController.java
@@ -555,40 +555,68 @@ public class CostCheckOrderController extends Controller {
     	for(int i=0;i<orderIdsArr.length;i++){
             if("提货".equals(orderNoArr[i])){
             	rec1 = Db.findFirst("select sum(amount) sum_amount from pickup_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.pickup_order_id = ? and fi.type = '应付'", orderIdsArr[i]);
-            	if(rec1.getDouble("sum_amount")!=null){
-            		totalamount = totalamount + rec1.getDouble("sum_amount");
+            	if(rec1!=null){
+            		if(rec1.getDouble("sum_amount")!=null){
+                		totalamount = totalamount + rec1.getDouble("sum_amount");
+                	}
             	}
             	rec = Db.findFirst("select sum(change_amount) change_amount from pickup_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.pickup_order_id = ?", orderIdsArr[i]);
-            	if(rec.getDouble("change_amount")!=null){
-            		changeamount = changeamount + rec.getDouble("change_amount");
+            	if(rec!=null){
+            		if(rec.getDouble("change_amount")!=null){
+                		changeamount = changeamount + rec.getDouble("change_amount");
+                	}
             	}
-            }else if("零担".equals(orderNoArr[i])){
+            }else if("零担".equals(orderNoArr[i])){ System.out.println(orderIdsArr[i]);
             	rec1 = Db.findFirst("select sum(amount) sum_amount from depart_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.depart_order_id = ? and fi.type = '应付'", orderIdsArr[i]);
-            	totalamount = totalamount + rec1.getDouble("sum_amount");
+            	if(rec1!=null){
+            		if(rec1.getDouble("sum_amount")!=null){
+            			totalamount = totalamount + rec1.getDouble("sum_amount");
+                	}
+            	}
             	rec = Db.findFirst("select sum(change_amount) change_amount from depart_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.depart_order_id = ?", orderIdsArr[i]);
-            	if(rec.getDouble("change_amount")!=null){
-            		changeamount = changeamount + rec.getDouble("change_amount");
+            	if(rec!=null){
+            		if(rec.getDouble("change_amount")!=null){
+            			changeamount = changeamount + rec.getDouble("change_amount");
+                	}
             	}
             }else if("配送".equals(orderNoArr[i])){
             	rec1 = Db.findFirst("select sum(amount) sum_amount from delivery_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.order_id = ? and fi.type = '应付'", orderIdsArr[i]);
-            	totalamount = totalamount + rec1.getDouble("sum_amount");
+            	if(rec1!=null){
+            		if(rec1.getDouble("sum_amount")!=null){
+            			totalamount = totalamount + rec1.getDouble("sum_amount");
+                	}
+            	}
             	rec = Db.findFirst("select sum(change_amount) change_amount from delivery_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.order_id = ?", orderIdsArr[i]);
-            	if(rec.getDouble("change_amount")!=null){
-            		changeamount = changeamount + rec.getDouble("change_amount");
+            	if(rec!=null){
+            		if(rec.getDouble("change_amount")!=null){
+            			changeamount = changeamount + rec.getDouble("change_amount");
+                	}
             	}
             }else if("成本单".equals(orderNoArr[i])){
             	rec1 = Db.findFirst("select sum(amount) sum_amount from arap_misc_cost_order_item amcoi left join fin_item fi on fi.id = amcoi.fin_item_id  where amcoi.misc_order_id = ? and fi.type ='应付'",orderIdsArr[i]);
-            	totalamount = totalamount + rec1.getDouble("sum_amount");
+            	if(rec1!=null){
+            		if(rec1.getDouble("sum_amount")!=null){
+            			totalamount = totalamount + rec1.getDouble("sum_amount");
+                	}
+            	}
             	rec = Db.findFirst("select sum(change_amount) change_amount from arap_misc_cost_order_item amcoi left join fin_item fi on fi.id = amcoi.fin_item_id  where amcoi.misc_order_id = ?",orderIdsArr[i]);
-            	if(rec.getDouble("change_amount")!=null){
-            		changeamount = changeamount + rec.getDouble("change_amount");
+            	if(rec!=null){
+            		if(rec.getDouble("change_amount")!=null){
+                		changeamount = changeamount + rec.getDouble("change_amount");
+                	}
             	}
             }else{
             	rec1 = Db.findFirst("select ifnull(sum(insurance_amount),0) sum_amount from insurance_fin_item ifi left join fin_item fi on fi.id = ifi.fin_item_id  where ifi.insurance_order_id = ? and fi.type ='应付'",orderIdsArr[i]);
-            	totalamount = totalamount + rec1.getDouble("sum_amount");
+            	if(rec1!=null){
+            		if(rec1.getDouble("sum_amount")!=null){
+            			totalamount = totalamount + rec1.getDouble("sum_amount");
+                	}
+            	}
             	rec = Db.findFirst("select sum(change_amount) change_amount from insurance_fin_item ifi left join fin_item fi on fi.id = ifi.fin_item_id  where ifi.insurance_order_id = ?",orderIdsArr[i]);
-            	if(rec.getDouble("change_amount")!=null){
-            		changeamount = changeamount + rec.getDouble("change_amount");
+            	if(rec!=null){
+            		if(rec.getDouble("change_amount")!=null){
+                		changeamount = changeamount + rec.getDouble("change_amount");
+                	}
             	}
             }
             
