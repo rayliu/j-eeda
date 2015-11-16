1fdd56eb0341af5fd331a2f14b866d708061fe02
doukevin <kevin.dou@eeda123.com>
2015-11-16 17:41:22 +0800

    页码添加全部页码类型

--- WebRoot/yh/js/arap/AccountAuditLog/list.js
+++ WebRoot/yh/js/arap/AccountAuditLog/list.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     	"bFilter": false, //不需要默认的搜索框
         "bSort": true, 
         "iDisplayLength": 100,
+        "aLengthMenu": [ [10, 25, 50, 100, 9999999], [10, 25, 50, 100, "All"] ],
         "sDom": "<'row-fluid'<'span6'l><'span6'f>r><'datatable-scroll't><'row-fluid'<'span12'i><'span12 center'p>>",
         "bServerSide": true,
     	"oLanguage": {
