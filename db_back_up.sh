b6e28826ff63e9a660a139a1bc3e12f66443ab01
Ray Liu <liuyubie@gmail.com>
2015-11-20 11:08:12 +0800

    给crontab调用的自动备份数据的脚本

--- db_back_up.sh
+++ db_back_up.sh
@@ -2,7 +2,8 @@
 
 filename=`date +%Y%m%d-%H%M%S`_yh.sql
 
-mysqldump -h$1 -u$2 -p$3 eeda_yh>/var/db/$filename
+# $1:db IP   $2:use_name  $3:pwd  $4:db_name
+mysqldump -h$1 -u$2 -p$3 $4>/var/db/$filename
 
 # 参考 http://blog.csdn.net/liumangxiong/article/details/7084637
 #crontab修改
