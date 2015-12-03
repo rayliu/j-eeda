63780bc37ff2c77d1a7cad8d3c33d984db7c3409
sean <985643410@qq.com>
2015-12-03 11:43:00 +0800

    配送单状态

--- WebRoot/yh/js/delivery/list.js
+++ WebRoot/yh/js/delivery/list.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
             {"mDataProp":"OFFICE_NAME","sWidth":"80px"},//网点
             {"mDataProp":"TRANSFER_ORDER_NO","sWidth":"100px"},
             {"mDataProp":"CUSTOMER_ORDER_NO","sWidth":"100px"},//客户订单号
-            {"mDataProp":"STATU","sWidth":"70px"},//状态
+            {"mDataProp":"STATUS","sWidth":"70px"},//状态
             {"mDataProp":"CARGO_NATURE",
             	"sWidth":"80px",
             	"fnRender": function(obj) {
