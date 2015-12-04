b00239e89f890591639988f13aa6a4917d0253fa
sean <985643410@qq.com>
2015-12-04 10:20:34 +0800

    报销单收款单位增加供应商下拉列表

--- WebRoot/yh/js/arap/CostReimbursement/edit.js
+++ WebRoot/yh/js/arap/CostReimbursement/edit.js
@@ -74,7 +74,83 @@ $(document).ready(function() {
  			}
 	 	},'json');
  	};
-	
+ 	//获取未审核供应商的list，选中信息在下方展示其他信息
+    $('#account_name').on('click input', function(){
+        var me = this;
+        var inputStr = $('#account_name').val();
+        var spList2 =$("#account_list");
+        $.get('/transferOrder/searchSp', {input:inputStr}, function(data){
+            if(inputStr!=$('#account_name').val()){//查询条件与当前输入值不相等，返回
+                return;
+            }
+            spList2.empty();
+            for(var i = 0; i < data.length; i++){
+                var abbr = data[i].ABBR;
+                var company_name = data[i].COMPANY_NAME;
+                var contact_person = data[i].CONTACT_PERSON;
+                var phone = data[i].PHONE;
+                
+                if(abbr == null) 
+                    abbr = '';
+                if(company_name == null)
+                    company_name = '';
+                if(contact_person == null)
+                    contact_person = '';
+                if(phone == null)
+                    phone = '';
+                
+                spList2.append("<li><a tabindex='-1' class='fromLocationItem' chargeType='"+data[i].CHARGE_TYPE+"' partyId='"+data[i].PID+"' post_code='"+data[i].POSTAL_CODE+"' contact_person='"+data[i].CONTACT_PERSON+"' email='"+data[i].EMAIL+"' phone='"+data[i].PHONE+"' spid='"+data[i].ID+"' address='"+data[i].ADDRESS+"', company_name='"+data[i].COMPANY_NAME+"', >"+abbr+" "+company_name+" "+contact_person+" "+phone+"</a></li>");
+            }
+            spList2.css({ 
+                left:$(me).position().left+"px", 
+                top:$(me).position().top+28+"px" 
+            }); 
+            spList2.show();
+            
+        },'json');
+        
+        
+    });
+    
+    // 没选中供应商，焦点离开，隐藏列表
+    $('#account_name').on('blur', function(){
+        $('#account_list').hide();
+    });
+
+    //当用户只点击了滚动条，没选供应商，再点击页面别的地方时，隐藏列表
+    $('#account_list').on('blur', function(){
+        $('#account_list').hide();
+    });
+
+    $('#account_list').on('mousedown', function(){
+        return false;//阻止事件回流，不触发 $('#spMessage').on('blur'
+    });
+
+    // 选中供应商
+    $('#account_list').on('mousedown', '.fromLocationItem', function(e){
+        console.log($('#account_list').is(":focus"));
+        var message = $(this).text();
+        $('#account_name').val(message.substring(0, message.indexOf(" ")));
+        var pageSpName = $("#pageSpName");
+        pageSpName.empty();
+        var pageSpAddress = $("#pageSpAddress");
+        pageSpAddress.empty();
+        pageSpAddress.append($(this).attr('address'));
+        var contact_person = $(this).attr('contact_person');
+        if(contact_person == 'null')
+            contact_person = '';
+        pageSpName.append(contact_person+'&nbsp;');
+        var phone = $(this).attr('phone');
+        if(phone == 'null')
+            phone = '';
+        pageSpName.append(phone); 
+        pageSpAddress.empty();
+        var address = $(this).attr('address');
+        if(address == 'null')
+            address = '';
+        pageSpAddress.append(address);
+        $('#account_list').hide();
+    });
 	//点击保存
 	$("#saveExpenseAccount").click(function(e){
 		saveCarSummaryData();
