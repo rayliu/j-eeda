995ba9e79fa09a702482519506a86389b50dfd46
Ray Liu <liuyubie@gmail.com>
2016-01-18 11:34:37 +0800

    微信：回单签收上传按钮激活

--- WebRoot/yh/js/wx/uploadFile.js
+++ WebRoot/yh/js/wx/uploadFile.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
 			if(data.length ==1){
 				var returnId = data[0].ID;
 				$('#orderDesc').text('回单确认存在，请从相册中选择照片上传');
+				$("#uploadBtn").attr("class", "weui_btn weui_btn_primary");
+				$("#uploadBtn").attr("disabled", false);
 				$("#uploadDesc").text("");
 				$('#returnId').val(returnId);	
 			}else if(data.length >1) {
