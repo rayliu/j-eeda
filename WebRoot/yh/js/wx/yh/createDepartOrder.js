5a4462660f130ec9137e164138878c37661caeaa
Kevin <kevin.dou@eeda123.com>
2016-02-23 15:54:44 +0800

    根据基础数据客户的时效带到微信发车单 创建的预计到达时间

--- WebRoot/yh/js/wx/yh/createDepartOrder.js
+++ WebRoot/yh/js/wx/yh/createDepartOrder.js
@@ -7,6 +7,22 @@ $(document).ready(function() {
 		$('#car_type_input').val(car_type);
 	}
 	
+	var limitation= $('#limitation').val();
+	$('#arrival_time').val(getNewDay($.format.date(new Date(), "yyyy-MM-dd"),limitation));
+
+
+	function getNewDay(dateTemp, days) {  
+	    var dateTemp = dateTemp.split("-");  
+	    var nDate = new Date(dateTemp[1] + '-' + dateTemp[2] + '-' + dateTemp[0]); //转换为MM-DD-YYYY格式    
+	    var millSeconds = Math.abs(nDate) + (days * 24 * 60 * 60 * 1000);  
+	    var rDate = new Date(millSeconds);  
+	    var year = rDate.getFullYear();  
+	    var month = rDate.getMonth() + 1;  
+	    if (month < 10) month = "0" + month;  
+	    var date = rDate.getDate();  
+	    if (date < 10) date = "0" + date;  
+	    return (year + "-" + month + "-" + date);  
+	}  
 	
 	var checkSp = function(){
 		var sp = $('#sp').val();
