896df717f39b5f8bacbd76c52275e45b1ff41c18
Kevin <kevin.dou@eeda123.com>
2016-06-13 15:30:51 +0800

    微信登录优化

--- src/controllers/yh/wx/WxController.java
+++ src/controllers/yh/wx/WxController.java
@@ -290,9 +290,9 @@ public class WxController extends ApiController {
          
         if(user != null){
         	UsernamePasswordToken token = new UsernamePasswordToken(user_name, pwd);
-            //token.setRememberMe(true);
+            token.setRememberMe(true);
             currentUser.login(token);
-            currentUser.getSession().setTimeout(-1000L);
+            //currentUser.getSession().setTimeout(-1000L);
             user.set("wechat_openid", openid).update();
             redirect(redirect+"?openid="+openid);
         }else{
