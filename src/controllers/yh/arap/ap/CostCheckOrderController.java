c27a9525ab50fb96926be37c8b7c17a0bb49edf6
Kevin <kevin.dou@eeda123.com>
2016-08-15 17:00:25 +0800

    应付对账单明细列表单品数量查询BUG

--- src/controllers/yh/arap/ap/CostCheckOrderController.java
+++ src/controllers/yh/arap/ap/CostCheckOrderController.java
@@ -1281,7 +1281,7 @@ public class CostCheckOrderController extends Controller {
         	orderByStr = " order by A."+colName+" "+sortBy;
         }
         
-		String searchSql = "select distinct dor.id,dofi.id did,dor.order_no order_no,dor.status,c.abbr spname,c1.abbr customer_name,IFNULL(c2.address,IFNULL(toid.notify_party_company,'')) receivingunit,(select count(*) from delivery_order_item doit where doit.delivery_id = dor.id) amount,ifnull(prod.volume,toi.volume) volume,ifnull(prod.weight,toi.weight) weight,dor.create_stamp create_stamp,ul.user_name creator,"
+		String searchSql = "select distinct dor.id,dofi.id did,dor.order_no order_no,dor.status,c.abbr spname,c1.abbr customer_name,IFNULL(c2.address,IFNULL(toid.notify_party_company,'')) receivingunit,(select sum(amount) from delivery_order_item doit where doit.delivery_id = dor.id) amount,ifnull(prod.volume,toi.volume) volume,ifnull(prod.weight,toi.weight) weight,dor.create_stamp create_stamp,ul.user_name creator,"
 				+ "	'配送' business_type,'' booking_note_number,"
 				+ " ifnull((SELECT NAME FROM location WHERE CODE = dor.route_from),'') route_from,ifnull((SELECT NAME FROM location WHERE CODE = dor.route_to),'') route_to,"
 				+ " (select ifnull(sum(amount),0) from delivery_order_fin_item dofi left join fin_item fi on fi.id = dofi.fin_item_id where dofi.order_id = dor.id and fi.type = '应付' and IFNULL(dofi.cost_source,'') != '对账调整金额') pay_amount,(SELECT ROUND(ifnull(sum(amount),0),2) FROM delivery_order_fin_item dofi WHERE dofi.order_id = dor.id) change_amount,"
