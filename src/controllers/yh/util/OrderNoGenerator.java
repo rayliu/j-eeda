663045b9e0d8b7a8d4af2b85d9dc5520aa56765c
Ray Liu <liuyubie@gmail.com>
2019-09-11 14:24:47 +0800

    单号生成去掉流水号, 用UUID生成

--- src/controllers/yh/util/OrderNoGenerator.java
+++ src/controllers/yh/util/OrderNoGenerator.java
@@ -2,6 +2,7 @@ package controllers.yh.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import models.yh.profile.CustomizeField;
 
@@ -22,9 +23,15 @@ public class OrderNoGenerator {
 	private static String dateValue = "20110101";
 	
 	public final static Byte[] locks = new Byte[0];  
+	
+	//根据UUID来成单号, 流水号逻辑作废
+	public static String getNextOrderNo(String orderPrefix) {
+	    return getOrderNoByUUId(orderPrefix);
+	}
+	
 	//如果服务器重启了，当前的序列号就从数据库找到最后的号码，然后接着计数
 	//TODO：如果需要按每张单的前缀来生成序列号，可以多加一个Map来记录
-	public static String getNextOrderNo(String orderPrefix) {
+	public static String getNextOrderNo1(String orderPrefix) {
 		if("00000".equals(count)){
 			initCountFromDB();
 		}
@@ -90,4 +97,16 @@ public class OrderNoGenerator {
 		}
 	}
 
+    private static String getOrderNoByUUId(String prefix){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String nowdate = sdf.format(new Date());
+        //int machineId = 1;//最大支持1-9个集群机器部署
+        int hashCodev = UUID.randomUUID().toString().hashCode();
+        //System.out.println(UUID.randomUUID().toString());
+        if(hashCodev < 0){
+            hashCodev = -hashCodev;//有可能是负数
+        }
+        //"%015d"的意思：0代表不足位数的补0，这样可以确保相同的位数，15是位数也就是要得到到的字符串长度是15，d代表数字。
+        return prefix+nowdate+"-"+String.format("%010d", hashCodev);
+    }
 }
